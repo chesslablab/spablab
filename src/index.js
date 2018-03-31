37e0ce4d9d10a34dcb27e8af623debbca87f02b4
programarivm <info@programarivm.com>
2018-03-31 22:36:12 +0100

    Refactoring this.state.pieces

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const WHITE = 'w';
-const BLACK = 'b';
-const BISHOP = 'B';
-const KING = 'K';
-const KNIGHT = 'N';
-const PAWN = 'P';
-const QUEEN = 'Q';
-const ROOK = 'R';
-const CASTLING_SHORT = 'O-O';
-const CASTLING_LONG = 'O-O-O';
+const Symbol = {
+    WHITE: 'w',
+    BLACK: 'b',
+    BISHOP: 'B',
+    KING: 'K',
+    KNIGHT: 'N',
+    PAWN: 'P',
+    QUEEN: 'Q',
+    ROOK: 'R',
+    CASTLING_SHORT: 'O-O',
+    CASTLING_LONG: 'O-O-O'
+}
 
 class Square extends React.Component {
   renderPiece(square) {
     if (square in this.props.state.pieces) {
      return (
-       this.props.state.pieces[square].piece
+       this.props.state.pieces[square].piece.unicode
      );
     }
   }
@@ -36,178 +38,249 @@ class Board extends React.Component {
     super(props);
 
     this.state = {
+      move: {
+        color: null,
+        piece: null,
+        symbol: null,
+        square: {
+          from: null,
+          to: null
+        }
+      },
       pieces: {
-        // white pieces
         a1: {
-          color: WHITE,
-          piece: '♖',
-          symbol: ROOK
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♖',
+            symbol: Symbol.ROOK
+          }
         },
         b1: {
-          color: WHITE,
-          piece: '♘',
-          symbol: KNIGHT
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♘',
+            symbol: Symbol.KNIGHT
+          }
         },
         c1: {
-          color: WHITE,
-          piece: '♗',
-          symbol: BISHOP
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♗',
+            symbol: Symbol.BISHOP
+          }
         },
         d1: {
-          color: WHITE,
-          piece: '♕',
-          symbol: QUEEN
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♕',
+            symbol: Symbol.QUEEN
+          }
         },
         e1: {
-          color: WHITE,
-          piece: '♔',
-          symbol: KING
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♔',
+            symbol: Symbol.KING
+          }
         },
         f1: {
-          color: WHITE,
-          piece: '♗',
-          symbol: BISHOP
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♗',
+            symbol: Symbol.BISHOP
+          }
         },
         g1: {
-          color: WHITE,
-          piece: '♘',
-          symbol: KNIGHT
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♘',
+            symbol: Symbol.KNIGHT
+          }
         },
         h1: {
-          color: WHITE,
-          piece: '♖',
-          symbol: ROOK
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♖',
+            symbol: Symbol.ROOK
+          }
         },
         a2: {
-          color: WHITE,
-          piece: '♙',
-          symbol: PAWN
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♙',
+            symbol: Symbol.PAWN
+          }
         },
         b2: {
-          color: WHITE,
-          piece: '♙',
-          symbol: PAWN
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♙',
+            symbol: Symbol.PAWN
+          }
         },
         c2: {
-          color: WHITE,
-          piece: '♙',
-          symbol: PAWN
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♙',
+            symbol: Symbol.PAWN
+          }
         },
         d2: {
-          color: WHITE,
-          piece: '♙',
-          symbol: PAWN
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♙',
+            symbol: Symbol.PAWN
+          }
         },
         e2: {
-          color: WHITE,
-          piece: '♙',
-          symbol: PAWN
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♙',
+            symbol: Symbol.PAWN
+          }
         },
         f2: {
-          color: WHITE,
-          piece: '♙',
-          symbol: PAWN
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♙',
+            symbol: Symbol.PAWN
+          }
         },
         g2: {
-          color: WHITE,
-          piece: '♙',
-          symbol: PAWN
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♙',
+            symbol: Symbol.PAWN
+          }
         },
         h2: {
-          color: WHITE,
-          piece: '♙',
-          symbol: PAWN
+          color: Symbol.WHITE,
+          piece: {
+            unicode: '♙',
+            symbol: Symbol.PAWN
+          }
         },
-        // black pieces
         a8: {
-          color: BLACK,
-          piece: '♜',
-          symbol: ROOK
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♜',
+            symbol: Symbol.ROOK
+          }
         },
         b8: {
-          color: BLACK,
-          piece: '♞',
-          symbol: KNIGHT
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♞',
+            symbol: Symbol.KNIGHT
+          }
         },
         c8: {
-          color: BLACK,
-          piece: '♝',
-          symbol: BISHOP
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♝',
+            symbol: Symbol.BISHOP
+          }
         },
         d8: {
-          color: BLACK,
-          piece: '♛',
-          symbol: QUEEN
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♛',
+            symbol: Symbol.QUEEN
+          }
         },
         e8: {
-          color: BLACK,
-          piece: '♚',
-          symbol: KING
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♚',
+            symbol: Symbol.KING
+          }
         },
         f8: {
-          color: BLACK,
-          piece: '♝',
-          symbol: BISHOP
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♝',
+            symbol: Symbol.BISHOP
+          }
         },
         g8: {
-          color: BLACK,
-          piece: '♞',
-          symbol: KNIGHT
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♞',
+            symbol: Symbol.KNIGHT
+          }
         },
         h8: {
-          color: BLACK,
-          piece: '♜',
-          symbol: ROOK
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♜',
+            symbol: Symbol.ROOK
+          }
         },
         a7: {
-          color: BLACK,
-          piece: '♟',
-          symbol: PAWN
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♟',
+            symbol: Symbol.PAWN
+          }
         },
         b7: {
-          color: BLACK,
-          piece: '♟',
-          symbol: PAWN
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♟',
+            symbol: Symbol.PAWN
+          }
         },
         c7: {
-          color: BLACK,
-          piece: '♟',
-          symbol: PAWN
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♟',
+            symbol: Symbol.PAWN
+          }
         },
         d7: {
-          color: BLACK,
-          piece: '♟',
-          symbol: PAWN
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♟',
+            symbol: Symbol.PAWN
+          }
         },
         e7: {
-          color: BLACK,
-          piece: '♟',
-          symbol: PAWN
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♟',
+            symbol: Symbol.PAWN
+          }
         },
         f7: {
-          color: BLACK,
-          piece: '♟',
-          symbol: PAWN
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♟',
+            symbol: Symbol.PAWN
+          }
         },
         g7: {
-          color: BLACK,
-          piece: '♟',
-          symbol: PAWN
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♟',
+            symbol: Symbol.PAWN
+          }
         },
         h7: {
-          color: BLACK,
-          piece: '♟',
-          symbol: PAWN
+          color: Symbol.BLACK,
+          piece: {
+            unicode: '♟',
+            symbol: Symbol.PAWN
+          }
         }
       }
     };
   }
 
   switchColor(color) {
-    if (color === BLACK) {
-      return WHITE;
+    if (color === Symbol.BLACK) {
+      return Symbol.WHITE;
     } else {
-      return BLACK;
+      return Symbol.BLACK;
     }
   }
 
@@ -217,9 +290,9 @@ class Board extends React.Component {
     let row = [];
 
     if (number % 2 !== 0) {
-      color = BLACK;
+      color = Symbol.BLACK;
     } else {
-      color = WHITE;
+      color = Symbol.WHITE;
     }
 
     for (let i=1; i<=8; i++) {
@@ -261,7 +334,7 @@ class Board extends React.Component {
 class Game extends React.Component {
   render() {
     return (
-      <Board/>
+      <Board className="board"/>
     );
   }
 }
@@ -270,5 +343,5 @@ class Game extends React.Component {
 
 ReactDOM.render(
   <Game />,
-  document.getElementById('root')
+  document.getElementById('pgn-chess-game')
 );
