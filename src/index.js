e100bb63a8bb18130e5c32b5e4d7543a6bf4e3e0
programarivm <info@programarivm.com>
2018-03-31 16:02:05 +0100

    Implementing the empty board...

--- src/index.js
+++ src/index.js
@@ -5,26 +5,185 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
+      <button className={['square', this.props.color].join(' ')} onClick={() => this.props.onClick()}>
+        {this.props.name}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pieces: [
+        {
+          piece: '♖',
+          square: 'a1'
+        },
+        {
+          piece: '♘',
+          square: 'b1'
+        },
+        {
+          piece: '♗',
+          square: 'c1'
+        },
+        {
+          piece: '♕',
+          square: 'd1'
+        },
+        {
+          piece: '♔',
+          square: 'e1'
+        },
+        {
+          piece: '♗',
+          square: 'f1'
+        },
+        {
+          piece: '♘',
+          square: 'g1'
+        },
+        {
+          piece: '♖',
+          square: 'h1'
+        },
+        {
+          piece: '♙',
+          square: 'a2'
+        },
+        {
+          piece: '♙',
+          square: 'b2'
+        },
+        {
+          piece: '♙',
+          square: 'c2'
+        },
+        {
+          piece: '♙',
+          square: 'd2'
+        },
+        {
+          piece: '♙',
+          square: 'e2'
+        },
+        {
+          piece: '♙',
+          square: 'f2'
+        },
+        {
+          piece: '♙',
+          square: 'g2'
+        },
+        {
+          piece: '♙',
+          square: 'h2'
+        },
+        {
+          piece: '♖',
+          square: 'a8'
+        },
+        {
+          piece: '♘',
+          square: 'b8'
+        },
+        {
+          piece: '♗',
+          square: 'c8'
+        },
+        {
+          piece: '♕',
+          square: 'd8'
+        },
+        {
+          piece: '♔',
+          square: 'e8'
+        },
+        {
+          piece: '♗',
+          square: 'f8'
+        },
+        {
+          piece: '♘',
+          square: 'g8'
+        },
+        {
+          piece: '♖',
+          square: 'h8'
+        },
+        {
+          piece: '♙',
+          square: 'a7'
+        },
+        {
+          piece: '♙',
+          square: 'b7'
+        },
+        {
+          piece: '♙',
+          square: 'c7'
+        },
+        {
+          piece: '♙',
+          square: 'd7'
+        },
+        {
+          piece: '♙',
+          square: 'e7'
+        },
+        {
+          piece: '♙',
+          square: 'f7'
+        },
+        {
+          piece: '♙',
+          square: 'g7'
+        },
+        {
+          piece: '♙',
+          square: 'h7'
+        }
+      ]
+    };
+  }
+
+  /* movePiece(i) {
+  } */
+
   render() {
     return (
       <div>
         <div className="board-row">
-          <Square/>
-          <Square/>
-          <Square/>
-          <Square/>
-          <Square/>
-          <Square/>
-          <Square/>
-          <Square/>
+          <Square name='a3' color='b' onClick={() => alert('foo')} />
+          <Square name='b3' color='w' onClick={() => alert('foo')} />
+          <Square name='c3' color='b' onClick={() => alert('foo')} />
+          <Square name='d3' color='w' onClick={() => alert('foo')} />
+          <Square name='e3' color='b' onClick={() => alert('foo')} />
+          <Square name='f3' color='w' onClick={() => alert('foo')} />
+          <Square name='g3' color='b' onClick={() => alert('foo')} />
+          <Square name='h3' color='w' onClick={() => alert('foo')} />
+        </div>
+        <div className="board-row">
+          <Square name='a2' color='w' onClick={() => alert('foo')} />
+          <Square name='b2' color='b' onClick={() => alert('foo')} />
+          <Square name='c2' color='w' onClick={() => alert('foo')} />
+          <Square name='d2' color='b' onClick={() => alert('foo')} />
+          <Square name='e2' color='w' onClick={() => alert('foo')} />
+          <Square name='f2' color='b' onClick={() => alert('foo')} />
+          <Square name='g2' color='w' onClick={() => alert('foo')} />
+          <Square name='h2' color='b' onClick={() => alert('foo')} />
+        </div>
+        <div className="board-row">
+          <Square name='a1' color='b' onClick={() => alert('foo')} />
+          <Square name='b1' color='w' onClick={() => alert('foo')} />
+          <Square name='c1' color='b' onClick={() => alert('foo')} />
+          <Square name='d1' color='w' onClick={() => alert('foo')} />
+          <Square name='e1' color='b' onClick={() => alert('foo')} />
+          <Square name='f1' color='w' onClick={() => alert('foo')} />
+          <Square name='g1' color='b' onClick={() => alert('foo')} />
+          <Square name='h1' color='w' onClick={() => alert('foo')} />
         </div>
       </div>
     );
