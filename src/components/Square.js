20e103c2c510986931571ce308031a1e7f99490f
programarivm <info@programarivm.com>
2018-04-05 09:43:39 +0100

    css update

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,16 +3,23 @@ import React from 'react';
 export default class Square extends React.Component {
   renderPiece(square) {
     if (square in this.props.state.pieces) {
-     return (
-       this.props.state.pieces[square].unicode
-     );
+      return (
+        this.props.state.pieces[square].unicode
+      );
+    }
+    else {
+      return (
+        ' '
+      );
     }
   }
 
   render() {
     return (
       <div className={['square', this.props.color].join(' ')} onClick={() => this.props.onClick()}>
-        <span>{this.renderPiece(this.props.square)}</span>
+        <span className={this.props.state.pieces[this.props.square] === undefined ? 'empty' : ''} >
+          {this.renderPiece(this.props.square)}
+        </span>
       </div>
     );
   }
