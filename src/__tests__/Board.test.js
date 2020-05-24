f479616cd5c87d3697891b065f03abd6e6301307
programarivm <info@programarivm.com>
2020-05-24 18:11:25 +0100

    pgn output generated by the app and the history are tested too; ruy lopez passes

--- src/__tests__/Board.test.js
+++ src/__tests__/Board.test.js
@@ -5,6 +5,7 @@ import Pgn from 'utils/Pgn.js';
 import TestRenderer from 'react-test-renderer';
 
 import BoardStore from 'stores/BoardStore.js';
+import HistoryStore from 'stores/HistoryStore.js';
 import ServerStore from 'stores/ServerStore.js';
 import SquareStore from 'stores/SquareStore.js';
 
@@ -41,24 +42,66 @@ describe('Square colors', () => {
 
 describe('Open Games', () => {
   it('Ruy Lopez', async () => {
-    const ws = await ServerStore.connect();
-    let game = [
+    // input given to the app
+    let moves = [
       {
-        move: { piece: { color: 'w', unicode: '♙', symbol: 'P' }, from: 'e2' },
+        piece: { color: 'w', unicode: '♙', symbol: 'P' },
+        from: 'e2',
         to: 'e4'
       },
       {
-        move: { piece: { color: 'b', unicode: '♟', symbol: 'P' }, from: 'e7' },
+        piece: { color: 'b', unicode: '♟', symbol: 'P' },
+        from: 'e7',
         to: 'e5'
+      },
+      {
+        piece: { color: 'w', unicode: '♘', symbol: 'N' },
+        from: 'g1',
+        to: 'f3'
+      },
+      {
+        piece: { color: 'b', unicode: '♞', symbol: 'N' },
+        from: 'b8',
+        to: 'c6'
       }
     ];
 
-    await SquareStore.click(game[0].move.from);
-    await SquareStore.click(game[0].to);
-    expect(BoardStore.state.pieces[game[0].to]).toEqual(game[0].move.piece);
+    // output calculated by the app
+    let pgns = [
+      'e4',
+      'e5',
+      'Nf3',
+      'Nc6'
+    ];
 
-    await SquareStore.click(game[1].move.from);
-    await SquareStore.click(game[1].to);
-    expect(BoardStore.state.pieces[game[1].to]).toEqual(game[1].move.piece);
+    const ws = await ServerStore.connect();
+
+    let move = await SquareStore.click(moves[0].from);
+    let pgn = await SquareStore.click(moves[0].to);
+
+    expect(pgn).toEqual(pgns[0]);
+    expect(BoardStore.state.pieces[moves[0].to]).toEqual(moves[0].piece);
+    expect(HistoryStore.state.items.pop().pgn).toEqual(pgn);
+
+    move = await SquareStore.click(moves[1].from);
+    pgn = await SquareStore.click(moves[1].to);
+
+    expect(pgn).toEqual(pgns[1]);
+    expect(BoardStore.state.pieces[moves[1].to]).toEqual(moves[1].piece);
+    expect(HistoryStore.state.items.pop().pgn).toEqual(pgn);
+
+    move = await SquareStore.click(moves[2].from);
+    pgn = await SquareStore.click(moves[2].to);
+
+    expect(pgn).toEqual(pgns[2]);
+    expect(BoardStore.state.pieces[moves[2].to]).toEqual(moves[2].piece);
+    expect(HistoryStore.state.items.pop().pgn).toEqual(pgn);
+
+    move = await SquareStore.click(moves[3].from);
+    pgn = await SquareStore.click(moves[3].to);
+
+    expect(pgn).toEqual(pgns[3]);
+    expect(BoardStore.state.pieces[moves[3].to]).toEqual(moves[3].piece);
+    expect(HistoryStore.state.items.pop().pgn).toEqual(pgn);
   });
 });
