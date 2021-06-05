52ec15af41ecb0fad85cf869538863141715ab35
programarivm <info@programarivm.com>
2021-06-05 18:41:45 +0200

    Wrapped the Board in a Provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from 'store';
 import Board from 'components/Board.js';
 import './index.css';
 
-var BoardElement = React.createElement(Board);
+// var BoardElement = React.createElement(Board);
+
+/* ReactDOM.render(
+  BoardElement,
+  document.getElementById('chess-board')
+); */
+
 
 ReactDOM.render(
-  BoardElement,
+  <Provider store={store}>
+    <Board />
+  </Provider>,
   document.getElementById('chess-board')
 );
