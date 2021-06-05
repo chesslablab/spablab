8d25bdcf0430b4ff505cbd0920620a8677c97595
programarivm <info@programarivm.com>
2021-06-05 18:58:01 +0200

    Added items to historyReducer

--- src/reducers/historyReducer.js
+++ src/reducers/historyReducer.js
@@ -1,6 +1,8 @@
 import historyActionTypes from 'constants/historyActionTypes';
 
-const initialState = {};
+const initialState = {
+  items: []
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
