99fcbefa0bf5e3fcb0cbab89310340d7d337c82b
programarivm <info@programarivm.com>
2021-06-06 20:36:48 +0200

    Implemented toFen()

--- src/utils/Board.js
+++ src/utils/Board.js
@@ -9,4 +9,35 @@ export default class Board {
     [ ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ', ' P ' ],
     [ ' R ', ' N ', ' B ', ' Q ', ' K ', ' B ', ' N ', ' R ' ]
   ];
+
+  static toFen = (ascii) => {
+    let string;
+    ascii.forEach((item, i) => {
+      string += item.join('').replace(' ', '');
+      if (i !== 0) {
+        string += '/';
+      }
+    });
+
+    return this.filter(string);
+  }
+
+  static filter = (string) => {
+    let filtered = '';
+    let strSplit = string.split('');
+    let n = 1;
+    strSplit.forEach((item, i) => {
+      if (strSplit[i] === '.' && strSplit[i+1] === '.') {
+          n++;
+      } else if (strSplit[i] === '.' && strSplit[i+1] !== '.') {
+          filtered += n;
+          n = 1;
+      } else if (strSplit[i] !== '.') {
+          filtered += strSplit[i];
+          n = 1;
+      }
+    });
+
+    return filtered;
+  }
 }
