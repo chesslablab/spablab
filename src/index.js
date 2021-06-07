a29ed7d519a2648bfa0026f5cbdb131a2c7bec37
programarivm <info@programarivm.com>
2021-06-07 15:31:31 +0200

    Preparing for npm package creation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
-import ReactDOM from 'react-dom';
-import Chess from 'components/Chess.js';
-
-ReactDOM.render(<Chess />, document.getElementById('react-chess'));
+export { Chess } from 'components/Chess';
