c73e9c3956d59b56c523705e8e3e9c65bbd45be3
programarivm <info@programarivm.com>
2021-06-07 22:40:53 +0200

    Cleaned up store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,6 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
-/* const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-); */
-
 const store = createStore(rootReducer);
 
 export default store;
