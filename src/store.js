ec4b3a9439aad7c051495a6b157c6724d3434f0f
programarivm <info@programarivm.com>
2021-06-09 10:18:36 +0200

    Applied middleware thunk in store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
