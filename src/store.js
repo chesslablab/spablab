cc6e7fb19306373fe8f70da4538db7288f5f6258
programarivm <info@programarivm.com>
2021-06-11 11:43:55 +0200

    Setup Redux DevTools

--- src/store.js
+++ src/store.js
@@ -1,7 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+// const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
