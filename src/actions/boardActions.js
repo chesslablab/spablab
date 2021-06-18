530a5c4efafe26985d6eddca7dbd390e205021b0
programarivm <info@programarivm.com>
2021-06-18 12:58:44 +0200

    Implemented validateMove() in boardActions.js

--- src/actions/boardActions.js
+++ src/actions/boardActions.js
@@ -1,5 +1,6 @@
 import boardActionTypes from '../constants/boardActionTypes';
 import historyActionTypes from '../constants/historyActionTypes';
+import { playfen } from '../actions/serverActions';
 
 export const start = (payload) => dispatch => {
   dispatch({
@@ -35,3 +36,25 @@ export const undo = () => ({
 export const browseHistory = () => ({
   type: boardActionTypes.BROWSE_HISTORY
 });
+
+export const validateMove = (state) => dispatch => {
+  if (!state.board.picked && state.board.fen) {
+    dispatch({
+      type: boardActionTypes.VALIDATE_MOVE
+    });
+    return new Promise((resolve, reject) => {
+      dispatch(playfen(state.server.ws, state.board.fen)).then((data) => {
+        const playfen = JSON.parse(data).playfen;
+        if (!playfen.legal) {
+          dispatch({
+            type: boardActionTypes.UNDO
+          });
+        }
+        resolve(true);
+      }).catch(err => {
+        dispatch({ type: serverActionTypes.CONNECTION_ERROR });
+        reject(err);
+      });
+    });
+  }
+};
--- src/components/MoveValidator.js
+++ src/components/MoveValidator.js
@@ -1,20 +1,12 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { playfen } from '../actions/serverActions';
-import { undo } from '../actions/boardActions';
+import { validateMove } from '../actions/boardActions';
 
 const MoveValidator = ({props}) => {
   const state = useSelector(state => state);
   const dispatch = useDispatch();
 
-  if (!state.board.picked && state.board.fen) {
-    dispatch(playfen(state.server.ws, state.board.fen)).then((data) => {
-      const playfen = JSON.parse(data).playfen;
-      if (!playfen.legal) {
-        dispatch(undo());
-      }
-    });
-  }
+  dispatch(validateMove(state));
 
   return null;
 }
