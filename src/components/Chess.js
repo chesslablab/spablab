a73d695ac346d0a56978f811cdce8be8beb61a94
programarivm <info@programarivm.com>
2021-06-28 11:01:10 +0200

    Replaced components/Buttons with components/PrimaryButtons

--- src/components/Chess.js
+++ src/components/Chess.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../store';
-import Buttons from './Buttons.js';
+import PrimaryButtons from './PrimaryButtons.js';
 import Board from './Board.js';
 import History from './History';
 import MoveValidator from './MoveValidator.js';
@@ -10,7 +10,7 @@ import '../index.css';
 const Chess = ({props}) => {
   return (
     <Provider store={store}>
-      <Buttons props={props} />
+      <PrimaryButtons props={props} />
       <Board props={props} />
       <History />
       {props.server ? <MoveValidator /> : null}
