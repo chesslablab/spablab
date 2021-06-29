7afbb7e0421d64cbd856bd7bb5e78d86fce75673
programarivm <info@programarivm.com>
2021-06-29 23:47:58 +0200

    Moved the server connection button from PrimaryButtons to Settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Button, Menu, MenuItem } from '@material-ui/core';
+import { Button, Divider, Menu, MenuItem } from '@material-ui/core';
 import SettingsIcon from '@material-ui/icons/Settings';
 import { useDispatch, useSelector } from 'react-redux';
-import { flipBoard } from '../actions/boardActions';
+import { startBoard, flipBoard } from '../actions/boardActions';
+import { analysis, connect } from '../actions/serverActions';
 
 const Settings = ({props}) => {
   const state = useSelector(state => state);
@@ -18,6 +19,44 @@ const Settings = ({props}) => {
     setAnchorEl(null);
   };
 
+  const menuItems = () => {
+    let items = [];
+
+    if (props.server) {
+        items.push(
+          <MenuItem
+            key={0}
+            onClick={() => {
+              dispatch(connect(props.server.host, props.server.port)).then((ws) => {
+                  dispatch(analysis(ws)).then(() => {
+                      dispatch(startBoard({ back: state.board.history.length - 1 }));
+                      handleClose();
+                    });
+                });
+            }}
+          >
+            Connect
+          </MenuItem>
+      );
+    }
+
+    items.push(<Divider key={1} />);
+
+    items.push(
+      <MenuItem
+        key={2}
+        onClick={() => {
+          dispatch(flipBoard());
+          handleClose();
+        }}
+      >
+        Flip board
+      </MenuItem>
+    );
+
+    return items;
+  }
+
   return (
     <div>
       <Button
@@ -32,11 +71,7 @@ const Settings = ({props}) => {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem
-          onClick={() => dispatch(flipBoard())}
-        >
-          Flip board
-        </MenuItem>
+        {menuItems()}
       </Menu>
     </div>
   );
