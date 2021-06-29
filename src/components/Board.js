40e1fdc3c5ae73e64d3b96b66bcf7eadf50d5834
Jordi Bassaganas <info@programarivm.com>
2021-06-29 16:07:54 +0200

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { pickPiece, leavePiece } from "../actions/boardActions";
-import Ascii from "../utils/Ascii";
-import Pgn from "../utils/Pgn";
-import Piece from "../utils/Piece";
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { pickPiece, leavePiece } from '../actions/boardActions';
+import Ascii from '../utils/Ascii';
+import Pgn from '../utils/Pgn';
+import Piece from '../utils/Piece';
 
-const Board = ({ props }) => {
-  const state = useSelector((state) => state);
+const Board = ({props}) => {
+  const state = useSelector(state => state);
   const dispatch = useDispatch();
 
   const board = () => {
@@ -19,47 +19,35 @@ const Board = ({ props }) => {
     ).forEach((rank, i) => {
       let row = [];
       rank.forEach((piece, j) => {
-        let payload = { piece: piece };
-        (i + k) % 2 !== 0
-          ? (color = Pgn.symbol.BLACK)
-          : (color = Pgn.symbol.WHITE);
-        state.board.flip === Pgn.symbol.WHITE
-          ? (payload = { ...payload, i: i, j: j })
-          : (payload = { ...payload, i: 7 - i, j: 7 - j });
-        row.push(
-          <div
-            key={k}
-            className={["square", color].join(" ")}
-            onClick={() =>
-              state.board.picked
-                ? dispatch(leavePiece(payload))
-                : dispatch(pickPiece(payload))
-            }
-          >
-            <span tabIndex={k}>{Piece.unicode[piece].char}</span>
-          </div>
-        );
-        k++;
+          let payload = { piece: piece };
+          (i + k) % 2 !== 0
+            ? color = Pgn.symbol.BLACK
+            : color = Pgn.symbol.WHITE;
+          state.board.flip === Pgn.symbol.WHITE
+            ? payload = {...payload, i: i, j: j}
+            : payload = {...payload, i: 7 - i, j: 7 - j};
+          row.push(<div
+              key={k}
+              className={['square', color].join(' ')}
+              onClick={() => state.board.picked ? dispatch(leavePiece(payload)) : dispatch(pickPiece(payload))}>
+              <span tabindex={k}>
+                {Piece.unicode[piece].char}
+              </span>
+            </div>
+          );
+          k++;
       });
-      rows.push(
-        <div key={i} className="board-row">
-          {row}
-        </div>
-      );
+      rows.push(<div key={i} className="board-row">{row}</div>);
     });
 
     return rows;
-  };
+  }
 
   return (
-    <div
-      className={["board", state.history.back !== 0 ? "past" : "present"].join(
-        " "
-      )}
-    >
+    <div className={['board', state.history.back !== 0 ? 'past' : 'present'].join(' ')}>
       {board()}
     </div>
   );
-};
+}
 
 export default Board;
