641b09dc537de753739cbfb24df3c3e9119cf958
programarivm <info@programarivm.com>
2021-06-29 20:23:50 +0200

    components/SecondaryButtons is replaced with components/Settings

--- src/components/Chess.js
+++ src/components/Chess.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from '../store';
 import PrimaryButtons from './PrimaryButtons.js';
 import InviteFriendDialog from './InviteFriendDialog';
-import SecondaryButtons from './SecondaryButtons.js';
+import Settings from './Settings.js';
 import Board from './Board.js';
 import History from './History';
 import MoveValidator from './MoveValidator.js';
 import '../index.css';
+import store from '../store';
 
 const Chess = ({props}) => {
   return (
     <Provider store={store}>
+      <Settings props={props} />
       <PrimaryButtons props={props} />
       <InviteFriendDialog />
       <Board props={props} />
       <History />
       {props.server ? <MoveValidator /> : null}
-      <SecondaryButtons props={props} />
     </Provider>
   );
 }
