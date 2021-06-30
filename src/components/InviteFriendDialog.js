286e61664d547dd0a1aebb7405e26462e17f66e4
programarivm <info@programarivm.com>
2021-06-30 11:22:37 +0200

    Designed 'Invite a friend' dialog

--- src/components/InviteFriendDialog.js
+++ src/components/InviteFriendDialog.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, InputLabel,
-  MenuItem, TextField, Select } from '@material-ui/core';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, MenuItem,
+  TextField, Select } from '@material-ui/core';
 import { useDispatch, useSelector } from "react-redux";
 import { close as closeInviteFriendDialog } from "../actions/inviteFriendDialogActions";
 import { startBoard } from '../actions/boardActions';
@@ -29,8 +29,7 @@ const InviteFriendDialog = () => {
     });
   }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleCreateCode = () => {
     let color = inviteFriendDialogState.color;
     if (color === "rand") {
       color = randColor();
@@ -44,52 +43,75 @@ const InviteFriendDialog = () => {
     });
   }
 
+  const handlePlay = () => {
+    console.log('TODO');
+  }
+
   return (
     <div>
-      <Dialog open={state.inviteFriendDialog.open} aria-labelledby="form-dialog-title">
+      <Dialog open={state.inviteFriendDialog.open}>
         <DialogTitle id="form-dialog-title">Invite a friend</DialogTitle>
         <DialogContent>
-          <form onSubmit={handleSubmit}>
-            <InputLabel>Color</InputLabel>
-            <Select
-              name="color"
-              fullWidth
-              required
-              defaultValue="rand"
-              onChange={handleChange}
-            >
-              <MenuItem value="" disabled>
-                Select a color
-              </MenuItem>
-              <MenuItem key={0} value="rand">
-                Random
-              </MenuItem>
-              <MenuItem key={1} value={Pgn.symbol.WHITE}>
-                White
-              </MenuItem>
-              <MenuItem key={2} value={Pgn.symbol.BLACK}>
-                Black
-              </MenuItem>
-            </Select>
-            <InputLabel>Minutes</InputLabel>
-            <TextField
-              name="time"
-              type="number"
-              fullWidth
-              required
-              defaultValue={10}
-              inputProps={{ min: "1", max: "60", step: "1" }}
-              onChange={handleChange}
-            />
-            <DialogActions>
-              <Button color="primary" type="submit">
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <TextField
+                select
+                fullWidth
+                name="color"
+                label="Color"
+                variant="outlined"
+                defaultValue="rand"
+                onChange={handleChange}
+              >
+                <MenuItem key={0} value="rand">
+                  Random
+                </MenuItem>
+                <MenuItem key={1} value={Pgn.symbol.WHITE}>
+                  White
+                </MenuItem>
+                <MenuItem key={2} value={Pgn.symbol.BLACK}>
+                  Black
+                </MenuItem>
+              </TextField>
+              <TextField
+                fullWidth
+                type="number"
+                name="time"
+                label="Minutes"
+                variant="outlined"
+                defaultValue={10}
+                inputProps={{ min: "1", max: "60", step: "1" }}
+                onChange={handleChange}
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => handleCreateCode()}
+              >
                 Create code
               </Button>
-              <Button onClick={() => dispatch(closeInviteFriendDialog())} color="primary">
-                Cancel
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Code"
+                variant="outlined"
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => handlePlay()}
+              >
+                Play
               </Button>
-            </DialogActions>
-          </form>
+            </Grid>
+          </Grid>
+          <DialogActions>
+            <Button
+              onClick={() => dispatch(closeInviteFriendDialog())} color="primary"
+            >
+              Cancel
+            </Button>
+          </DialogActions>
         </DialogContent>
       </Dialog>
     </div>
