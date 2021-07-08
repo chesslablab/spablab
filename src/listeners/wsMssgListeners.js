ed406ee16ceba1b18e16c79882c75cba585f9172
programarivm <info@programarivm.com>
2021-07-08 17:17:26 +0200

    Fixed wsMssgListeners

--- src/listeners/wsMssgListeners.js
+++ src/listeners/wsMssgListeners.js
@@ -15,9 +15,10 @@ export const wsMssgListeners = (data) => dispatch => {
       dispatch(onAccept(data));
       break;
     case '/playfen' === cmd:
-      if (store.getState().mode.name === 'playfriend' && store.getState().mode.playfriend.jwt_decoded.color !== data['/playfen'].turn
-      ) {
-        dispatch({ type: boardActionTypes.TOGGLE_TURN });
+      if (modeNames.PLAYFRIEND === store.getState().mode.current) {
+        if (store.getState().mode.playfriend.color !== data['/playfen'].turn) {
+          dispatch({ type: boardActionTypes.TOGGLE_TURN });
+        }
       }
       dispatch(onPlayfen(data));
       break;
@@ -30,15 +31,17 @@ export const wsMssgListeners = (data) => dispatch => {
 };
 
 export const onStartPlayfriend = (data) => dispatch => {
+  const jwtDecoded = jwt_decode(data['/start'].jwt);
   dispatch({
     type: modeActionTypes.SET,
     payload: {
       current: modeNames.PLAYFRIEND,
       playfriend: {
         jwt: data['/start'].jwt,
-        jwt_decoded: jwt_decode(data['/start'].jwt),
+        jwt_decoded: jwtDecoded,
         hash: data['/start'].hash,
-        created_code: true
+        created_code: true,
+        color: jwtDecoded.color
       }
     }
   });
@@ -46,6 +49,7 @@ export const onStartPlayfriend = (data) => dispatch => {
 
 export const onAccept = (data) => dispatch => {
   if (!store.getState().mode.playfriend.created_code) {
+    const jwtDecoded = jwt_decode(data['/accept'].jwt);
     dispatch({
       type: modeActionTypes.SET,
       payload: {
@@ -54,7 +58,8 @@ export const onAccept = (data) => dispatch => {
           jwt: data['/accept'].jwt,
           jwt_decoded: jwt_decode(data['/accept'].jwt),
           hash: data['/accept'].hash,
-          created_code: false
+          created_code: false,
+          color: jwtDecoded.color === Pgn.symbol.WHITE ? Pgn.symbol.BLACK : Pgn.symbol.WHITE
         }
       }
     });
--- src/reducers/modeReducer.js
+++ src/reducers/modeReducer.js
@@ -7,7 +7,8 @@ const initialState = {
     jwt: null,
     jwt_decoded: null,
     hash: null,
-    created_code: false
+    created_code: false,
+    color: null
   }
 };
 
