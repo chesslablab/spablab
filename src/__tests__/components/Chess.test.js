fd82af338b08a5b4d9b665b74c670234d59722cb
programarivm <info@programarivm.com>
2021-07-14 12:40:33 +0200

    Passed the test 'is rendered'

--- src/__tests__/components/Chess.test.js
+++ src/__tests__/components/Chess.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Chess from 'components/Chess';
 
+const props = {
+  server: {
+    host: '127.0.0.1',
+    port: '8080'
+  }
+};
+
 describe("Chess", () => {
-  it("should render", () => {
-    const wrapper = mount(<Chess />);
+  it("is rendered", () => {
+    const wrapper = mount(<Chess props={props} />);
   });
 });
