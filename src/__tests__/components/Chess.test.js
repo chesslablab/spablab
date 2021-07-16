ac2e5925ad449b4eb4353148f9e17c87146ee8ef
programarivm <info@programarivm.com>
2021-07-16 15:48:39 +0200

    Added test: 'flips the board'

--- src/__tests__/components/Chess.test.js
+++ src/__tests__/components/Chess.test.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import { mount } from 'enzyme';
+import React, { useEffect } from 'react';
 import Chess from 'components/Chess';
+import { useDispatch, useSelector } from 'react-redux';
+import { renderHook } from '@testing-library/react-hooks';
+import { mount } from 'enzyme';
+import boardActionTypes from 'constants/boardActionTypes';
+import store from 'store';
+
+import { Provider } from 'react-redux';
+
+const Dispatcher = (action) => {
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(action);
+  }, [dispatch]);
+
+  return { state }
+};
 
 const props = {
   server: {
@@ -9,8 +26,20 @@ const props = {
   }
 };
 
+const wrapper = ({ children }) => (
+  <Provider store={store}>{children}</Provider>
+)
+
 describe("Chess", () => {
   it("is rendered", () => {
     const wrapper = mount(<Chess props={props} />);
+  });
+  it("flips the board", () => {
+    expect(store.getState().board.flip).toBe('w');
+    const action = {
+      type: boardActionTypes.FLIP
+    }
+    const { result } = renderHook(() => Dispatcher(action), { wrapper })
+    expect(result.current.state.board.flip).toBe('b');
   });
 });
