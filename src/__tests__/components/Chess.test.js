d71d79e41a4f9245c556a1500695de4cf58d260c
programarivm <info@programarivm.com>
2021-07-20 13:22:01 +0200

    Chess.test.js passed

--- src/__tests__/components/Chess.test.js
+++ src/__tests__/components/Chess.test.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Provider } from 'react-redux';
 import Chess from 'components/Chess';
 import { useDispatch, useSelector } from 'react-redux';
 import { renderHook } from '@testing-library/react-hooks';
@@ -6,9 +7,14 @@ import { mount } from 'enzyme';
 import boardActionTypes from 'constants/boardActionTypes';
 import store from 'store';
 
-import { Provider } from 'react-redux';
+const props = {
+  server: {
+    host: '127.0.0.1',
+    port: '8080'
+  }
+};
 
-const Dispatcher = (action) => {
+const ReduxDispatcher = (action) => {
   const state = useSelector(state => state);
   const dispatch = useDispatch();
 
@@ -19,27 +25,26 @@ const Dispatcher = (action) => {
   return { state }
 };
 
-const props = {
-  server: {
-    host: '127.0.0.1',
-    port: '8080'
-  }
-};
-
 const wrapper = ({ children }) => (
   <Provider store={store}>{children}</Provider>
 )
 
 describe("Chess", () => {
   it("is rendered", () => {
-    const wrapper = mount(<Chess props={props} />);
+    const chess = mount(<Chess props={props} />);
   });
-  it("flips the board", () => {
+  it("the first chess board square is a black rook before flipping the chess board", () => {
+    const chess = mount(<Chess props={props} />);
+    const text = chess.find('.board-row').at(0).find('.square').at(0).find('span').text();
     expect(store.getState().board.flip).toBe('w');
-    const action = {
-      type: boardActionTypes.FLIP
-    }
-    const { result } = renderHook(() => Dispatcher(action), { wrapper })
+    expect(text).toEqual('♜');
+  });
+  it("the first chess board square is a white rook after flipping the chess board", () => {
+    const action = { type: boardActionTypes.FLIP };
+    const { result } = renderHook(() => ReduxDispatcher(action), { wrapper });
+    const chess = mount(<Chess props={props} />);
+    const text = chess.find('.board-row').at(0).find('.square').at(0).find('span').text();
     expect(result.current.state.board.flip).toBe('b');
+    expect(text).toEqual('♖');
   });
 });
