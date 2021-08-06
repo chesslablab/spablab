4b650d5f68dc32282b126282dc86ee9e8687bd24
programarivm <info@programarivm.com>
2021-08-06 17:52:16 +0200

    Removed unused actions

--- src/actions/createInviteCodeDialogActions.js
+++
@@ -1,9 +0,0 @@
-import createInviteCodeDialogActions from '../constants/createInviteCodeDialogActionTypes';
-
-export const close = () => ({
-  type: createInviteCodeDialogActions.CLOSE
-});
-
-export const open = () => ({
-  type: createInviteCodeDialogActions.OPEN
-});
