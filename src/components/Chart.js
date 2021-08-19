33716828451e1a06639b316cf2447bce09519791
programarivm <info@programarivm.com>
2021-08-19 18:02:55 +0200

    Implemented Chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,33 +2,47 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Legend, LineChart, Line, XAxis, YAxis } from 'recharts';
 
-let data = {
-  material: [],
+const getDimensions = (items) => {
+  return Object.keys(items).map(item => item.split("\\").pop());
 };
 
-const Chart = () => {
-  const state = useSelector(state => state);
+const initData = (dim) => {
+  let data = {};
+  dim.forEach(item => data[item] = []);
+  return data;
+};
 
-  if (state.heuristicPictureDialog.heuristic_picture) {
-    state.heuristicPictureDialog.heuristic_picture.balance.forEach((item) => {
-      data.material.push({
-        material: item[0]
-      });
-    });
-  } else {
-    data = {
-      material: []
-    };
-  }
-
-  return(
-    <LineChart width={400} height={400} data={data.material}>
+const buildCharts = (data) => {
+  return Object.keys(data).map(key =>
+    <LineChart key={key} width={300} height={200} data={data[key]}>
       <XAxis dataKey="n" />
       <YAxis />
       <Legend />
-      <Line type="monotone" dataKey="material" stroke="#007a99" dot={false} strokeWidth={2} />
+      <Line type="monotone" dataKey={key} stroke="#007a99" dot={false} strokeWidth={2} />
     </LineChart>
   );
+};
+
+let dim = [];
+
+let data = {};
+
+const Chart = () => {
+  const state = useSelector(state => state);
+  if (state.heuristicPictureDialog.heuristic_picture) {
+    dim = getDimensions(state.heuristicPictureDialog.heuristic_picture.dimensions);
+    data = initData(dim);
+    state.heuristicPictureDialog.heuristic_picture.balance.forEach((item, i) => {
+      dim.forEach((dimension, j) => {
+        data[dimension].push({
+          [dimension]: item[j]
+        });
+      });
+    });
+    return <div>{buildCharts(data)}</div>;
+  }
+
+  return null;
 }
 
 export default Chart;
