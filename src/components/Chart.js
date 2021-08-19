d0ffc42f848f577c418740111717d81a3e428a2b
programarivm <info@programarivm.com>
2021-08-19 15:16:16 +0200

    Plotted material

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,55 +1,32 @@
 import React from 'react';
-import { LineChart, Line } from 'recharts';
+import { useSelector } from 'react-redux';
+import { Legend, LineChart, Line, XAxis, YAxis } from 'recharts';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+let data = {
+  material: [],
+};
 
 const Chart = () => {
+  const state = useSelector(state => state);
+
+  if (state.heuristicPictureDialog.heuristic_picture) {
+    state.heuristicPictureDialog.heuristic_picture.balance.forEach((item) => {
+      data.material.push({
+        material: item[0]
+      });
+    });
+  } else {
+    data = {
+      material: []
+    };
+  }
+
   return(
-    <LineChart width={400} height={400} data={data}>
-      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+    <LineChart width={400} height={400} data={data.material}>
+      <XAxis dataKey="n" />
+      <YAxis />
+      <Legend />
+      <Line type="monotone" dataKey="material" stroke="#007a99" dot={false} strokeWidth={2} />
     </LineChart>
   );
 }
