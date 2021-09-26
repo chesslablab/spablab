307e57f97e91dc2a189ff250a76b64349291da53
Divyateja Pasupuleti <divyateja2004@gmail.com>
2021-09-26 15:13:20 +0530

    Add a button to propose a takeback #132

--- src/components/Chess.js
+++ src/components/Chess.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import { CssBaseline } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
@@ -14,6 +14,7 @@ import Buttons from './Buttons.js';
 import History from './History';
 import MainBreadcrumbs from './MainBreadcrumbs';
 import MoveValidator from './MoveValidator.js';
+import ButtonsPlayFriendMode from './ButtonsPlayFriendMode.js';
 import Timers from './Timers';
 import InfoAlert from './InfoAlert.js';
 import '../index.css';
@@ -36,6 +37,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Chess = ({props}) => {
+  const state = useSelector(state => state);
   const classes = useStyles();
 
   return (
@@ -60,8 +62,8 @@ const Chess = ({props}) => {
           <Timers />
           <Paper elevation={3} className={classes.paper}>
             <MoveValidator />
+            <ButtonsPlayFriendMode />
           </Paper>
-          <Button variant="outlined">Propose a Takeback</Button>
           <InfoAlert />
         </Grid>
       </Grid>
