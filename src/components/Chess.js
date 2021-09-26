9dcbbc085d4e4c48c8e4549b1ccbc687b6d5ac2c
Jordi Bassaganas <info@programarivm.com>
2021-09-26 12:02:02 +0200

    Update Chess.js

--- src/components/Chess.js
+++ src/components/Chess.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
 import { CssBaseline } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import LoadFenDialog from './LoadFenDialog';
