28c75bd1c1c03c8cd3ed2cd1c701cbc074440e60
Divyateja Pasupuleti <divyateja2004@gmail.com>
2021-09-26 15:30:23 +0530

    Add a button to propose a takeback #132

--- src/components/Chess.js
+++ src/components/Chess.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Provider, useDispatch, useSelector } from 'react-redux';
+import { Provider } from 'react-redux';
 import { CssBaseline } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
