a7a85d8898c9fcbab94cd9133032f5bf65635e70
programarivm <info@programarivm.com>
2021-10-09 12:43:41 +0200

    Implemented action wsMssgDraw()

--- src/components/OfferDrawDialog.js
+++ src/components/OfferDrawDialog.js
@@ -1,40 +1,30 @@
 import React, { useState } from "react";
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@material-ui/core';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
 import { useDispatch, useSelector } from "react-redux";
-import offerDrawDialogActions from '../constants/offerDrawDialogActionTypes';
-import { wsMssgAccept, wsMssgQuit } from '../actions/serverActions';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-    },
-  },
-}));
+import offerDrawDialogActionTypes from '../constants/offerDrawDialogActionTypes';
+import { wsMssgDraw } from '../actions/serverActions';
 
 const OfferDrawDialog = () => {
   const state = useSelector(state => state);
   const dispatch = useDispatch();
-  const classes = useStyles();
 
   const handleOfferDraw = (event) => {
     event.preventDefault();
-    // TODO
-    console.log('TODO');
-    dispatch({ type: offerDrawDialogActions.OPEN });
+    wsMssgDraw(state, 'propose').then((data) => {
+      dispatch({ type: offerDrawDialogActionTypes.CLOSE });
+    });
   }
 
   return (
     <Dialog open={state.offerDrawDialog.open} maxWidth="sm" fullWidth={true}>
       <DialogTitle>Offer draw</DialogTitle>
       <DialogContent>
-        <form className={classes.root} onSubmit={handleOfferDraw}>
+        <form onSubmit={handleOfferDraw}>
           <DialogActions>
             <Button type="submit">
               Accept
             </Button>
-            <Button onClick={() => dispatch({ type: offerDrawDialogActions.CLOSE })}>
+            <Button onClick={() => dispatch({ type: offerDrawDialogActionTypes.CLOSE })}>
               Cancel
             </Button>
           </DialogActions>
