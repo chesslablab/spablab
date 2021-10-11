3140e3f9e7a35fc92f7c67aea3b13505e4877340
programarivm <info@programarivm.com>
2021-10-11 11:34:33 +0200

    Fixed TakebackOfferDialog

--- src/components/TakebackOfferDialog.js
+++ src/components/TakebackOfferDialog.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
 import { useDispatch, useSelector } from "react-redux";
 import { wsMssgTakeback } from '../actions/serverActions';
-import takebackOfferDialogActionTypes from '../constants/takebackAcceptDialogActionTypes';
+import takebackOfferDialogActionTypes from '../constants/takebackOfferDialogActionTypes';
 import modeActionTypes from '../constants/modeActionTypes';
 
 const TakebackOfferDialog = () => {
