d71dbfb001cd8ad5ffb86863d57ca71805454272
programarivm <info@programarivm.com>
2021-12-04 13:50:54 +0100

    Added the GameOverButtons component

--- src/components/PlayFriendMode/Buttons.js
+++ src/components/PlayFriendMode/Buttons.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import GameButtons from './GameButtons';
+import GameOverButtons from './GameOverButtons';
 
 const Buttons = () => {
   return (
     <div>
       <GameButtons />
+      <GameOverButtons />
     </div>
   );
 }
