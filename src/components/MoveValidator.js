8c037c18bfd6853ba321347f7e79c40e0a8f6662
programarivm <info@programarivm.com>
2021-12-29 17:37:11 +0100

    Cleaned up the MoveValidator component

--- src/components/MoveValidator.js
+++ src/components/MoveValidator.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { wsMssgPlayfen } from '../actions/serverActions';
 import Paper from '@material-ui/core/Paper';
@@ -7,42 +7,28 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableRow from '@material-ui/core/TableRow';
+import Movetext from '../utils/Movetext.js';
 
 const MoveValidator = ({props}) => {
   const state = useSelector(state => state);
-  const [rows, setRows] = useState([]);
 
   if (state.board.fen) {
     wsMssgPlayfen(state);
   }
 
-  useEffect(() => {
-    if (state.board.movetext) {
-      let movetext = state.board.movetext.split(' ');
-      let last = movetext[movetext.length - 1];
-      let secondLast = movetext[movetext.length - 2];
-      let newRows = rows.map(item => ({...item}));
-      if (movetext.length % 2 !== 0) {
-        newRows.push({ w: last.split('.')[1], b: '' });
-      } else {
-        newRows.pop();
-        newRows.push({ w: secondLast.split('.')[1], b: last });
-      }
-      setRows(newRows);
-    }
-  }, [state.board.movetext]);
-
   return (
     <TableContainer component={Paper}>
       <Table aria-label="simple table">
         <TableBody>
-          {rows.map((row, i) => (
-            <TableRow key={i}>
-              <TableCell align="right">{i + 1}</TableCell>
-              <TableCell align="right">{row.w}</TableCell>
-              <TableCell align="right">{row.b}</TableCell>
-            </TableRow>
-          ))}
+          {
+            Movetext.toRows(state.board.movetext).map((row, i) => (
+              <TableRow key={i}>
+                <TableCell align="right">{i + 1}</TableCell>
+                <TableCell align="right">{row.w}</TableCell>
+                <TableCell align="right">{row.b}</TableCell>
+              </TableRow>
+            ))
+          }
         </TableBody>
       </Table>
     </TableContainer>
--- src/utils/Movetext.js
+++ src/utils/Movetext.js
@@ -1,15 +1,17 @@
 export default class Movetext {
   static toRows = (string) => {
-    let rows = [];
-    const arr = string.split(' ');
-    arr.map((item, i) => {
-      if (i % 2 === 0) {
-        rows.push({
-          w: arr[i].split('.')[1],
-          b: arr[i+1] ? arr[i+1] : ''
-        });
-      }
-    });
+    let rows = [];    
+    if (string) {
+      const arr = string.split(' ');
+      arr.map((item, i) => {
+        if (i % 2 === 0) {
+          rows.push({
+            w: arr[i].split('.')[1],
+            b: arr[i+1] ? arr[i+1] : ''
+          });
+        }
+      });
+    }
 
     return rows;
   }
