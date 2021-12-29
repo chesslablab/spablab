69567763aa3ec8c900a7066a79236a18ebc56975
programarivm <info@programarivm.com>
2021-12-29 17:03:51 +0100

    Passed test 'is 1.e4'

--- src/__tests__/utils/Movetext.test.js
+++ src/__tests__/utils/Movetext.test.js
@@ -1,6 +1,16 @@
 import Movetext from 'utils/Movetext.js';
 
 describe('toRows()', () => {
+  it('is 1.e4', () => {
+    const string = '1.e4';
+    const expected = [
+      {
+        w: 'e4',
+        b: '',
+      }
+    ];
+    expect(Movetext.toRows(string)).toEqual(expected);
+  });
   it('is 1.e4 e5', () => {
     const string = '1.e4 e5';
     const expected = [
