8861936a8dea45d150e1afa5899379ac69986659
programarivm <info@programarivm.com>
2022-01-04 11:34:05 +0100

    Fixed buttons in components/History

--- src/components/History.js
+++ src/components/History.js
@@ -12,31 +12,23 @@ const History = () => {
   const dispatch = useDispatch();
 
   return (
-    <ButtonGroup color="primary">
+    <ButtonGroup variant="outlined" aria-label="outlined button group">
       <Button
-        color="default"
-        size="small"
         startIcon={<FastRewindIcon />}
         disabled={state.board.history.length - 1 - Math.abs(state.history.back) === 0}
         onClick={() => dispatch(goToBeginning({ back: state.board.history.length - 1}))}
       />
       <Button
-        color="default"
-        size="small"
         startIcon={<SkipPreviousIcon />}
         disabled={state.board.history.length - 1 - Math.abs(state.history.back) === 0}
         onClick={() => dispatch(goBack())}
       />
       <Button
-        color="default"
-        size="small"
         startIcon={<SkipNextIcon />}
         disabled={state.history.back === 0}
         onClick={() => dispatch(goForward())}
       />
       <Button
-        color="default"
-        size="small"
         startIcon={<FastForwardIcon />}
         disabled={state.history.back === 0}
         onClick={() => dispatch(goToEnd())}
