f0dc29aa564bc629c633439a8bd92ad82e411c47
programarivm <info@programarivm.com>
2022-01-19 10:49:24 +0100

    Replaced ecoChessOpeningsDialogActionTypes with ecoOpeningsDialogActionTypes

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -12,7 +12,7 @@ import loadFenDialogActionTypes from '../constants/loadFenDialogActionTypes';
 import loadPgnDialogActionTypes from '../constants/loadPgnDialogActionTypes';
 import fenDialogActionTypes from '../constants/fenDialogActionTypes';
 import createInviteCodeDialogActionTypes from '../constants/createInviteCodeDialogActionTypes';
-import ecoChessOpeningsDialogActionTypes from '../constants/ecoChessOpeningsDialogActionTypes';
+import ecoOpeningsDialogActionTypes from '../constants/ecoOpeningsDialogActionTypes';
 import enterInviteCodeDialogActionTypes from '../constants/enterInviteCodeDialogActionTypes';
 import playLikeGrandmasterDialogActionTypes from '../constants/playLikeGrandmasterDialogActionTypes';
 import alertActionTypes from '../constants/alertActionTypes';
@@ -122,7 +122,7 @@ const Buttons = ({ props }) => {
       >
         <MenuItem
           onClick={() => {
-            dispatch({ type: ecoChessOpeningsDialogActionTypes.OPEN });
+            dispatch({ type: ecoOpeningsDialogActionTypes.OPEN });
             handleCloseTraining();
           }}
         >
--- src/constants/ecoChessOpeningsDialogActionTypes.js
+++
@@ -1,6 +0,0 @@
-const ecoChessOpeningsDialogActionTypes = {
-	CLOSE: 'CLOSE_ECO_CHESS_OPENINGS_DIALOG',
-	OPEN: 'OPEN_ECO_CHESS_OPENINGS_DIALOG'
-};
-
-export default ecoChessOpeningsDialogActionTypes;
