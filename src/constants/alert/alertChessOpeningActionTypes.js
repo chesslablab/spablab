2cf57c325d88d93341a1cb78d6040cb8433249ae
programarivm <info@programarivm.com>
2022-01-25 11:16:03 +0100

    Replaced chessOpeningActionTypes with alertChessOpeningActionTypes

---
+++ src/constants/alert/alertChessOpeningActionTypes.js
@@ -0,0 +1,6 @@
+const alertChessOpeningActionTypes = {
+  DISPLAY: 'DISPLAY_CHESS_OPENING_ALERT',
+  CLOSE: 'CLOSE_CHESS_OPENING_ALERT'
+};
+
+export default alertChessOpeningActionTypes;
--- src/constants/alert/chessOpeningActionTypes.js
+++
@@ -1,6 +0,0 @@
-const chessOpeningActionTypes = {
-  DISPLAY: 'DISPLAY_CHESS_OPENING_ALERT',
-  CLOSE: 'CLOSE_CHESS_OPENING_ALERT'
-};
-
-export default chessOpeningActionTypes;
