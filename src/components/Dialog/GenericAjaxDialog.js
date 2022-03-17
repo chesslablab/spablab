471b025c1f6d30715c8aa7cecbf02f065eafff91
programarivm <info@programarivm.com>
2022-03-17 14:15:51 +0100

    Replaced the animated gif with the LinearProgress component

--- src/components/Dialog/GenericAjaxDialog.js
+++ src/components/Dialog/GenericAjaxDialog.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Dialog, DialogContent } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import loading from '../../assets/img/loading.gif';
-
-const useStyles = makeStyles({
-  loading: {
-    textAlign: 'center',
-  },
-});
+import { Box, Dialog, DialogContent, LinearProgress } from '@mui/material';
 
 const GenericAjaxDialog = ({ props }) => {
-  const classes = useStyles();
   const state = useSelector(state => state);
 
   return (
     <Dialog open={state.genericAjaxDialog.open} maxWidth="sm" fullWidth={true}>
       <DialogContent>
-        <div className={classes.loading}>
-          <img src={loading} alt="loading" />
-        </div>
+        <Box sx={{ width: '100%' }}>
+          <LinearProgress />
+        </Box>
       </DialogContent>
     </Dialog>
   );
