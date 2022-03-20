29cfd17b438e79784ec0abddcaf541ab4ed75add
programarivm <info@programarivm.com>
2022-03-20 15:28:07 +0100

    Implemented Opening.byEco()

--- src/__tests__/utils/Opening.test.js
+++ src/__tests__/utils/Opening.test.js
@@ -1,6 +1,21 @@
 import Opening from 'utils/Opening.js';
 
 describe('byMovetext()', () => {
+  it('is 720 openings that start with A', () => {
+    expect(Opening.byEco('A').length).toBe(720);
+  });
+  it('is 722 openings that start with B', () => {
+    expect(Opening.byEco('B').length).toBe(722);
+  });
+  it('is 1168 openings that start with C', () => {
+    expect(Opening.byEco('C').length).toBe(1168);
+  });
+  it('is 497 openings that start with D', () => {
+    expect(Opening.byEco('D').length).toBe(497);
+  });
+  it('is 290 openings that start with D', () => {
+    expect(Opening.byEco('E').length).toBe(290);
+  });
   it('starts with foo', () => {
     const openings = Opening.startsWith('foo');
     expect(openings.length).toBe(0);
--- src/utils/Opening.js
+++ src/utils/Opening.js
@@ -1,6 +1,10 @@
 const openings = require('../assets/js/openings.json');
 
 export default class Opening {
+  static byEco = (eco) => {
+    return openings.filter(item => item.eco.startsWith(eco));
+  }
+
   static startsWith = (movetext) => {
     return openings.filter(item => item.movetext.startsWith(movetext));
   }
