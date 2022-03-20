b759a21d4805030e2af966c604f9d9da6e5ba7a5
programarivm <info@programarivm.com>
2022-03-20 15:32:16 +0100

    Renamed the tests

--- src/__tests__/utils/Opening.test.js
+++ src/__tests__/utils/Opening.test.js
@@ -1,28 +1,28 @@
 import Opening from 'utils/Opening.js';
 
 describe('byMovetext()', () => {
-  it('is 720 openings that start with A', () => {
+  it('is 720 starting with A', () => {
     expect(Opening.byEco('A').length).toBe(720);
   });
-  it('is 722 openings that start with B', () => {
+  it('is 722 starting with B', () => {
     expect(Opening.byEco('B').length).toBe(722);
   });
-  it('is 1168 openings that start with C', () => {
+  it('is 1168 starting with C', () => {
     expect(Opening.byEco('C').length).toBe(1168);
   });
-  it('is 497 openings that start with D', () => {
+  it('is 497 starting with D', () => {
     expect(Opening.byEco('D').length).toBe(497);
   });
-  it('is 290 openings that start with D', () => {
+  it('is 290 starting with D', () => {
     expect(Opening.byEco('E').length).toBe(290);
   });
-  it('is 0 openings that start with foo', () => {
+  it('is 0 starting with foo', () => {
     expect(Opening.byMovetext('foo').length).toBe(0);
   });
-  it('is 990 openings that start with 1.e4 e5', () => {
+  it('is 990 starting with 1.e4 e5', () => {
     expect(Opening.byMovetext('1.e4 e5').length).toBe(990);
   });
-  it('is 651 openings that starts with 1.e4 e5 2.Nf3', () => {
+  it('is 651 starting with 1.e4 e5 2.Nf3', () => {
     expect(Opening.byMovetext('1.e4 e5 2.Nf3').length).toBe(651);
   });
 });
