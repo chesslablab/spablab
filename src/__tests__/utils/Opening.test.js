d7713903e98bac995dce501358368f7e42c384c6
programarivm <info@programarivm.com>
2022-03-20 15:19:06 +0100

    Added tests

--- src/__tests__/utils/Opening.test.js
+++ src/__tests__/utils/Opening.test.js
@@ -1,8 +1,16 @@
 import Opening from 'utils/Opening.js';
 
 describe('byMovetext()', () => {
+  it('starts with foo', () => {
+    const openings = Opening.startsWith('foo');
+    expect(openings.length).toBe(0);
+  });
   it('starts with 1.e4 e5', () => {
     const openings = Opening.startsWith('1.e4 e5');
     expect(openings.length).toBe(990);
+  });
+  it('starts with 1.e4 e5 2.Nf3', () => {
+    const openings = Opening.startsWith('1.e4 e5 2.Nf3');
+    expect(openings.length).toBe(651);
   });
 });
