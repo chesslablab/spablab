65f4b4ea2984ec00af661118af9bade36bc9b039
programarivm <info@programarivm.com>
2022-05-03 11:08:31 +0200

    Refactored the CreateInviteCodeDialog component

--- src/components/Dialog/CreateInviteCodeDialog.js
+++ src/components/Dialog/CreateInviteCodeDialog.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, MenuItem, TextField } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 import { wsMssgQuit, wsMssgStartPlayfriend } from '../../actions/serverActions';
 import createInviteCodeDialogActionTypes from '../../constants/dialog/createInviteCodeDialogActionTypes';
 import Pgn from '../../utils/Pgn';
 
-const useStyles = makeStyles({
-  form: {
-    marginTop: 10,
-    '& .MuiTextField-root': {
-      padding: 15,
-    },
-  },
-});
-
 const CreateInviteCodeDialog = () => {
-  const classes = useStyles();
+  const state = useSelector(state => state);
+
+  return (
+    <Dialog open={state.createInviteCodeDialog.open} maxWidth="xs" fullWidth={true}>
+      <DialogTitle>Create invite code</DialogTitle>
+        {!state.mode.playfriend.hash ? <CreateCode /> : <CopyCode />}
+    </Dialog>
+  );
+}
+
+const CreateCode = () => {
   const state = useSelector(state => state);
   const dispatch = useDispatch();
 
@@ -32,67 +32,80 @@ const CreateInviteCodeDialog = () => {
   }
 
   return (
-    <Dialog open={state.createInviteCodeDialog.open} maxWidth="sm" fullWidth={true}>
-      <DialogTitle>Create invite code</DialogTitle>
-      <DialogContent>
-        <form className={classes.form} onSubmit={handleCreateCode}>
-          <TextField
-            select
-            fullWidth
-            name="color"
-            label="Color"
-            defaultValue="rand"
-          >
-            <MenuItem key={0} value="rand">
-              Random
-            </MenuItem>
-            <MenuItem key={1} value={Pgn.symbol.WHITE}>
-              White
-            </MenuItem>
-            <MenuItem key={2} value={Pgn.symbol.BLACK}>
-              Black
-            </MenuItem>
-          </TextField>
-          <TextField
-            fullWidth
-            type="number"
-            name="time"
-            label="Minutes"
-            defaultValue={10}
-            inputProps={{ min: "1", max: "60", step: "1" }}
-          />
-          <TextField
-            fullWidth
-            type="number"
-            name="increment"
-            label="Increment in seconds"
-            defaultValue={0}
-            inputProps={{ min: "0", max: "60", step: "1" }}
-          />
-          {
-            state.mode.playfriend.hash
-              ?  <TextField
-                    fullWidth
-                    type="text"
-                    name="sharecode"
-                    label="Share this code with a friend"
-                    value={state.mode.playfriend.hash}
-                  />
-              : null
-          }
-          <DialogActions>
-            {
-              !state.mode.playfriend.hash
-                ? <div>
-                    <Button type="submit">Create code</Button>
-                    <Button onClick={() => dispatch({ type: createInviteCodeDialogActionTypes.CLOSE })}>Cancel</Button>
-                  </div>
-                : <Button onClick={() => dispatch({ type: createInviteCodeDialogActionTypes.CLOSE })}>Play</Button>
-            }
-          </DialogActions>
-        </form>
-      </DialogContent>
-    </Dialog>
+    <DialogContent>
+      <form onSubmit={handleCreateCode}>
+        <TextField
+          select
+          fullWidth
+          margin="dense"
+          name="color"
+          label="Color"
+          defaultValue="rand"
+        >
+          <MenuItem key={0} value="rand">
+            Random
+          </MenuItem>
+          <MenuItem key={1} value={Pgn.symbol.WHITE}>
+            White
+          </MenuItem>
+          <MenuItem key={2} value={Pgn.symbol.BLACK}>
+            Black
+          </MenuItem>
+        </TextField>
+        <TextField
+          fullWidth
+          margin="dense"
+          type="number"
+          name="time"
+          label="Minutes"
+          defaultValue={5}
+          inputProps={{ min: "1", max: "60", step: "1" }}
+        />
+        <TextField
+          fullWidth
+          margin="dense"
+          type="number"
+          name="increment"
+          label="Increment in seconds"
+          defaultValue={0}
+          inputProps={{ min: "0", max: "60", step: "1" }}
+        />
+        <DialogActions>
+          <Button type="submit">
+            Create Code
+          </Button>
+          <Button onClick={() => dispatch({ type: createInviteCodeDialogActionTypes.CLOSE })}>
+            Cancel
+          </Button>
+        </DialogActions>
+      </form>
+    </DialogContent>
+  );
+}
+
+const CopyCode = () => {
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  return (
+    <DialogContent>
+      <TextField
+        fullWidth
+        margin="dense"
+        type="text"
+        name="sharecode"
+        label="Share this code with a friend"
+        value={state.mode.playfriend.hash}
+      />
+      <DialogActions>
+        <Button onClick={() => navigator.clipboard.writeText(state.mode.playfriend.hash)}>
+          Copy Code
+        </Button>
+        <Button onClick={() => dispatch({ type: createInviteCodeDialogActionTypes.CLOSE })}>
+          Play
+        </Button>
+      </DialogActions>
+    </DialogContent>
   );
 }
 
