ffbc9597289f1f7f68403a168fdb1774c95c1c3b
programarivm <info@programarivm.com>
2022-05-11 23:48:51 +0200

    Added videos

--- src/components/Dialog/WatchDialog.js
+++ src/components/Dialog/WatchDialog.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Alert, AppBar, Button, Dialog, IconButton, Toolbar, Typography, Slide } from '@mui/material/';
+import { AppBar, Button, Dialog, IconButton, Toolbar, Typography, Slide } from '@mui/material/';
 import CloseIcon from '@mui/icons-material/Close';
+import Videos from "../Videos.js";
 import { useDispatch, useSelector } from "react-redux";
 import watchDialogActionTypes from "../../constants/dialog/watchDialogActionTypes";
 
@@ -20,30 +21,24 @@ const WatchDialog = () => {
       TransitionComponent={Transition}
     >
       <AppBar sx={{ position: 'relative' }}>
-       <Toolbar>
-         <IconButton
-           edge="start"
-           color="inherit"
-           onClick={() => dispatch({ type: watchDialogActionTypes.CLOSE })}
-           aria-label="close"
-         >
-           <CloseIcon />
-         </IconButton>
-         <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-           Watch amazing videos!
-         </Typography>
-         <Button autoFocus color="inherit" onClick={() => dispatch({ type: watchDialogActionTypes.CLOSE })}>
-           Close
-         </Button>
-       </Toolbar>
-     </AppBar>
-     <Alert
-       className="info-alert"
-       severity="info"
-       style={{ margin: 15 }}
-     >
-       Would you want your videos to be listed here? Please follow <a href="https://twitter.com/programarivm" style={{ textDecoration: 'none' }}>@programarivm</a> on Twitter and send a DM. Thank you!
-     </Alert>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={() => dispatch({ type: watchDialogActionTypes.CLOSE })}
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+            Watch
+          </Typography>
+          <Button autoFocus color="inherit" onClick={() => dispatch({ type: watchDialogActionTypes.CLOSE })}>
+            Close
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Videos />
     </Dialog>
   );
 };
