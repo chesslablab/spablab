2c7c1fbb4d200cafd6d745c7da2f98d41c20413d
programarivm <info@programarivm.com>
2022-06-03 11:56:32 +0200

    Installed RTK

--- src/store.js
+++ src/store.js
@@ -1,11 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 export default store;
