2d02b14b0811c7800af17010e033b2465f02ec4a
programarivm <info@programarivm.com>
2022-06-05 10:26:09 +0200

    Implemented modeSlice

---
+++ src/features/modeSlice.js
@@ -0,0 +1,121 @@
+import modeNames from '../constants/modeNames';
+import Wording from '../utils/Wording.js';
+import Pgn from '../utils/Pgn';
+
+const initialState = {
+  current: modeNames.ANALYSIS,
+  grandmaster: {
+    color: null,
+    movetext: null
+  },
+  play: {
+    jwt: null,
+    jwt_decoded: null,
+    hash: null,
+    color: null,
+    takeback: null,
+    draw: null,
+    resign: null,
+    rematch: null,
+    leave: null,
+    accepted: false,
+    timer: {
+      expiry_timestamp: null,
+      over: null
+    }
+  }
+};
+
+const modeSlice = createSlice({
+  name: 'mode',
+  initialState: initialState,
+  reducers: {
+    modeSetAnalysis(state) {
+      state = initialState;
+    },
+    modeSetGrandmaster(state) {
+      state.current = modeNames.GRANDMASTER;
+      state.grandmaster.color = action.payload.color;
+    },
+    modeSetLoadFen(state) {
+      state.current = modeNames.LOADFEN;
+    },
+    modeSetLoadPgn(state) {
+      state.current = modeNames.LOADPGN;
+    },
+    modeSetPlay(state, action) {
+      state.current = action.payload.current;
+      state.play = action.payload.play;
+    },
+    modeGrandmasterMovetext(state, action) {
+      state.grandmaster.movetext = action.payload.movetext;
+    },
+    modePlayAccept(state) {
+      const expiryTimestamp = new Date();
+      expiryTimestamp.setSeconds(
+        expiryTimestamp.getSeconds() + parseInt(state.play.jwt_decoded.min) * 60
+      );
+      state.play.accepted = true;
+      state.play.timer.expiry_timestamp = expiryTimestamp;
+    },
+    modePlayTakebackAccept(state) {
+      state.play.takeback = Wording.verb.ACCEPT.toLowerCase();
+    },
+    modePlayTakebackDecline(state) {
+      state.play.takeback = null;
+    },
+    modePlayTakebackPropose(state) {
+      state.play.takeback = Wording.verb.PROPOSE.toLowerCase();
+    },
+    modePlayDrawAccept(state) {
+      state.play.draw = Wording.verb.ACCEPT.toLowerCase();
+    },
+    modePlayDrawDecline(state) {
+      state.play.draw = null;
+    },
+    modePlayDrawPropose(state) {
+      state.play.draw = Wording.verb.PROPOSE.toLowerCase();
+    },
+    modePlayResignAccept(state) {
+      state.play.resign = Wording.verb.ACCEPT.toLowerCase();
+    },
+    modePlayTimeOver(state, action) {
+      state.play.timer.over = action.payload.color;
+    },
+    modePlayRematchAccept(state) {
+      state.play.rematch = Wording.verb.ACCEPT.toLowerCase();
+    },
+    modePlayRematchDecline(state) {
+      state.play.rematch = null;
+    },
+    modePlayRematchPropose(state) {
+      state.play.rematch = Wording.verb.PROPOSE.toLowerCase();
+    },
+    modePlayLeaveAccept(state) {
+      state.play.leave = Wording.verb.ACCEPT.toLowerCase();
+    }
+  }
+});
+
+export const {
+  modeSetAnalysis,
+  modeSetGrandmaster,
+  modeSetLoadFen,
+  modeSetLoadPgn,
+  modeSetPlay,
+  modeGrandmasterMovetext,
+  modePlayAccept,
+  modePlayTakebackAccept,
+  modePlayTakebackDecline,
+  modePlayTakebackPropose,
+  modePlayDrawAccept,
+  modePlayDrawDecline,
+  modePlayDrawPropose,
+  modePlayResignAccept,
+  modePlayTimeOver,
+  modePlayRematchAccept,
+  modePlayRematchDecline,
+  modePlayRematchPropose,
+  modePlayLeaveAccept
+} = modeSlice.actions;
+export default modeSlice.reducer;
