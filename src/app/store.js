9db3b3e3fa28e473f674d915ef6e09d576313dda
programarivm <info@programarivm.com>
2022-06-05 13:52:47 +0200

    Fixed issue: A non-serializable value was detected in an action

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,10 @@ import rootReducer from './rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
