650848bd2f3c88f70de9d134cc2b1afb0ea6b279
programarivm <info@programarivm.com>
2022-06-05 10:30:08 +0200

    Removed Wording.extract()

--- src/utils/Wording.js
+++ src/utils/Wording.js
@@ -5,19 +5,10 @@ export default class Wording {
     PROPOSE: 'PROPOSE',
     // ...
   };
-
   static verbs = [
     Wording.verb.ACCEPT,
     Wording.verb.DECLINE,
     Wording.verb.PROPOSE,
     // ...
   ];
-
-  static extract = (haystack, needle) => {
-    if (haystack.split('_').includes(needle)) {
-      return needle;
-    }
-
-    return null;
-  }
 }
