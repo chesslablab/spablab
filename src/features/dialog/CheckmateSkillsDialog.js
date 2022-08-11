df2720ffdf9647971fd940730ecc314b14818146
programarivm <info@programarivm.com>
2022-08-11 18:24:32 +0200

    Refactored CheckmateSkillsDialog

--- src/features/dialog/CheckmateSkillsDialog.js
+++ src/features/dialog/CheckmateSkillsDialog.js
@@ -1,82 +1,108 @@
-import React from 'react';
+import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, MenuItem, TextField } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  IconButton,
+  MenuItem,
+  TextField
+} from '@mui/material';
 import Pgn from '../../common/Pgn';
-import { closeCheckmateSkillsDialog } from '../../features/dialog/checkmateSkillsDialogSlice';
 import { setTraining } from '../../features/mainButtonsSlice';
+import { closeCheckmateSkillsDialog } from '../../features/dialog/checkmateSkillsDialogSlice';
+import SelectColorButtons from '../../features/dialog/SelectColorButtons';
 import WsAction from '../../ws/WsAction';
 
 const CheckmateSkillsDialog = () => {
   const state = useSelector(state => state);
   const dispatch = useDispatch();
 
-  const handlePlay = (event) => {
-    event.preventDefault();
+  const checkmateTypes = ['Q', 'R', 'BB', 'BN'];
+
+  const [dialogData, setDialogData] = React.useState({
+    type: 'rand',
+    color: 'rand'
+  });
+
+  const handleCreateGame = () => {
+    let checkmateType;
+    let color;
+    dialogData.type === 'rand'
+      ? checkmateType = checkmateTypes[Math.floor(Math.random() * checkmateTypes.length)]
+      : checkmateType = dialogData.type;
+    dialogData.color === 'rand'
+      ? color = Math.random() < 0.5 ? Pgn.symbol.WHITE : Pgn.symbol.BLACK
+      : color = dialogData.color;
     dispatch(setTraining());
     dispatch(closeCheckmateSkillsDialog());
-    let color;
-    event.target.elements.color.value === 'rand'
-      ? color = Math.random() < 0.5 ? Pgn.symbol.WHITE : Pgn.symbol.BLACK
-      : color = event.target.elements.color.value;
-    WsAction.randomCheckmate(state, color, event.target.elements.type.value);
+    WsAction.randomCheckmate(state, color, checkmateType);
   }
+
+  const handleTypeChange = (event: Event, type) => {
+    setDialogData({
+      type: type.props.value,
+      color: dialogData.color
+    });
+  };
 
   return (
     <Dialog open={state.checkmateSkillsDialog.open} maxWidth="xs" fullWidth={true}>
-      <DialogTitle>Checkmate skills</DialogTitle>
+      <DialogTitle>
+        <Grid container>
+          <Grid item xs={11}>
+            Checkmate skills
+          </Grid>
+          <Grid item xs={1}>
+            <IconButton onClick={() => dispatch(closeCheckmateSkillsDialog())}>
+              <CloseIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </DialogTitle>
       <DialogContent>
-        <form onSubmit={handlePlay}>
-          <TextField
-            select
-            fullWidth
-            name="color"
-            label="Color"
-            defaultValue="rand"
-            margin="normal"
-          >
-            <MenuItem key={0} value="rand">
-              Random
-            </MenuItem>
-            <MenuItem key={1} value={Pgn.symbol.WHITE}>
-              White
-            </MenuItem>
-            <MenuItem key={2} value={Pgn.symbol.BLACK}>
-              Black
-            </MenuItem>
-          </TextField>
-          <TextField
-            select
-            fullWidth
-            required
-            name="type"
-            label="Select an option"
-            defaultValue=""
-            margin="normal"
-          >
-            <MenuItem value="" disabled>
-              Select an option
-            </MenuItem>
-            <MenuItem key={0} value="Q">
-              King and queen vs. king
-            </MenuItem>
-            <MenuItem key={1} value="R">
-              King and rook vs. king
-            </MenuItem>
-            <MenuItem key={2} value="BB">
-              King and two bishops vs. king
-            </MenuItem>
-            <MenuItem key={3} value="BN">
-              King and bishop and knight vs. king
-            </MenuItem>
-          </TextField>
-          <DialogActions>
-            <Button type="submit">Play</Button>
-            <Button onClick={() => dispatch(closeCheckmateSkillsDialog())}>
-              Cancel
-            </Button>
-          </DialogActions>
-        </form>
+        <TextField
+          select
+          fullWidth
+          name="type"
+          label="Select an endgame"
+          defaultValue="rand"
+          margin="normal"
+          onChange={handleTypeChange}
+        >
+          <MenuItem key={0} value="rand">
+            Random
+          </MenuItem>
+          <MenuItem key={1} value="Q">
+            King and queen vs. king
+          </MenuItem>
+          <MenuItem key={2} value="R">
+            King and rook vs. king
+          </MenuItem>
+          <MenuItem key={3} value="BB">
+            King and two bishops vs. king
+          </MenuItem>
+          <MenuItem key={4} value="BN">
+            King and bishop and knight vs. king
+          </MenuItem>
+        </TextField>
+        <Grid container justifyContent="center">
+          <SelectColorButtons props={dialogData} />
+        </Grid>
       </DialogContent>
+      <DialogActions>
+        <Button
+          fullWidth
+          variant="outlined"
+          onClick={() => handleCreateGame()}
+        >
+          Play
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 }
