40868b334d4b8d7293eb89861150443925caa442
programarivm <info@programarivm.com>
2022-08-11 18:42:57 +0200

    Refactored PlayGmDialog

--- src/features/dialog/PlayGmDialog.js
+++ src/features/dialog/PlayGmDialog.js
@@ -1,57 +1,67 @@
-import React from 'react';
+import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, MenuItem, TextField } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  IconButton
+} from '@mui/material';
 import Pgn from '../../common/Pgn';
-import { closePlayGmDialog } from '../../features/dialog/playGmDialogSlice';
 import { setTraining } from '../../features/mainButtonsSlice';
+import { closePlayGmDialog } from '../../features/dialog/playGmDialogSlice';
+import SelectColorButtons from '../../features/dialog/SelectColorButtons';
 import WsAction from '../../ws/WsAction';
 
 const PlayGmDialog = () => {
   const state = useSelector(state => state);
   const dispatch = useDispatch();
 
-  const handlePlay = (event) => {
-    event.preventDefault();
+  const [dialogData, setDialogData] = React.useState({
+    color: 'rand'
+  });
+
+  const handleCreateGame = () => {
     let color;
-    event.target.elements.color.value === 'rand'
+    dialogData.color === 'rand'
       ? color = Math.random() < 0.5 ? Pgn.symbol.WHITE : Pgn.symbol.BLACK
-      : color = event.target.elements.color.value;
-    WsAction.startGm(state, color);
+      : color = dialogData.color;
     dispatch(setTraining());
     dispatch(closePlayGmDialog());
+    WsAction.startGm(state, color);
   }
 
   return (
     <Dialog open={state.playGmDialog.open} maxWidth="xs" fullWidth={true}>
-      <DialogTitle>Guess the move</DialogTitle>
+      <DialogTitle>
+        <Grid container>
+          <Grid item xs={11}>
+            Guess the move
+          </Grid>
+          <Grid item xs={1}>
+            <IconButton onClick={() => dispatch(closePlayGmDialog())}>
+              <CloseIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </DialogTitle>
       <DialogContent>
-        <form onSubmit={handlePlay}>
-          <TextField
-            select
-            fullWidth
-            name="color"
-            label="Color"
-            defaultValue="rand"
-            margin="normal"
-          >
-            <MenuItem key={0} value="rand">
-              Random
-            </MenuItem>
-            <MenuItem key={1} value={Pgn.symbol.WHITE}>
-              White
-            </MenuItem>
-            <MenuItem key={2} value={Pgn.symbol.BLACK}>
-              Black
-            </MenuItem>
-          </TextField>
-          <DialogActions>
-            <Button type="submit">Play</Button>
-            <Button onClick={() => dispatch(closePlayGmDialog())}>
-              Cancel
-            </Button>
-          </DialogActions>
-        </form>
+        <Grid container justifyContent="center">
+          <SelectColorButtons props={dialogData} />
+        </Grid>
       </DialogContent>
+      <DialogActions>
+        <Button
+          fullWidth
+          variant="outlined"
+          onClick={() => handleCreateGame()}
+        >
+          Create Game
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 }
