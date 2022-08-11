5f9461b77ca052ed590d77ddc76e58a4b6107ba1
programarivm <info@programarivm.com>
2022-08-11 11:11:59 +0200

    Refactored PlayOnlineDialog

--- src/features/dialog/PlayOnlineDialog.js
+++ src/features/dialog/PlayOnlineDialog.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Pgn from '../../common/Pgn';
+import CloseIcon from '@mui/icons-material/Close';
 import {
-  Button,
+  Avatar,
+  ButtonGroup,
   Dialog,
-  DialogActions,
   DialogContent,
   DialogTitle,
-  MenuItem,
-  Paper,
+  Grid,
+  IconButton,
   Slider,
-  TextField,
   Typography
 } from '@mui/material';
+import wKing from '../../assets/img/pieces/png/150/wKing.png';
+import wbKing from '../../assets/img/pieces/png/150/wbKing.png';
+import bKing from '../../assets/img/pieces/png/150/bKing.png';
+import Pgn from '../../common/Pgn';
 import PlayOnlineTable from '../../features/table/PlayOnlineTable';
 import { closePlayOnlineDialog } from '../../features/dialog/playOnlineDialogSlice';
 import { setPlayOnline } from '../../features/mainButtonsSlice';
@@ -22,79 +25,104 @@ const PlayOnlineDialog = () => {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  const handleCreateCode = (event) => {
-    event.preventDefault();
+  const [minutes, setMinutes] = React.useState(5);
+  const [increment, setIncrement] = React.useState(3);
+
+  const handleMinutesChange = (event: Event, minutes: number) => {
+    setMinutes(minutes);
+  };
+
+  const handleIncrementChange = (event: Event, increment: number) => {
+    setIncrement(increment);
+  };
+
+  const handlePlay = (color) => {
     const settings = {
-      color: event.target.elements.color.value === 'rand'
-        ? Math.random() < 0.5 ? Pgn.symbol.WHITE : Pgn.symbol.BLACK
-        : event.target.elements.color.value,
-      min: event.target.elements.min.value,
-      increment: event.target.elements.increment.value,
+      color: color === 'rand' ? Math.random() < 0.5 ? Pgn.symbol.WHITE : Pgn.symbol.BLACK : color,
+      min: minutes,
+      increment: increment,
       submode: 'online'
     };
-    WsAction.startPlay(state, settings);
     dispatch(closePlayOnlineDialog());
     dispatch(setPlayOnline());
+    WsAction.startPlay(state, settings);
   }
 
   return (
     <Dialog open={state.playOnlineDialog.open} maxWidth="xs" fullWidth={true}>
-      <DialogTitle>Play online</DialogTitle>
+      <DialogTitle>
+        <Grid container>
+          <Grid item xs={11}>
+            Play online
+          </Grid>
+          <Grid item xs={1}>
+            <IconButton onClick={() => dispatch(closePlayOnlineDialog())}>
+              <CloseIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </DialogTitle>
       <DialogContent>
-        <form onSubmit={handleCreateCode}>
-          <Paper style={{paddingTop: 15, paddingLeft: 15, paddingRight: 15}}>
-            <Typography id="input-minutes" gutterBottom>
-              Minutes per side
-            </Typography>
-            <Slider
-              name="min"
-              aria-label="Minutes"
-              defaultValue={5}
-              valueLabelDisplay="auto"
-              step={1}
-              min={1}
-              max={60}
-            />
-            <Typography id="input-increment" gutterBottom>
-              Increment in seconds
-            </Typography>
-            <Slider
-              name="increment"
-              aria-label="Increment"
-              defaultValue={3}
-              valueLabelDisplay="auto"
-              step={1}
-              min={0}
-              max={60}
-            />
-            <TextField
-              select
-              fullWidth
-              margin="dense"
-              name="color"
-              label="Color"
-              defaultValue="rand"
+        <Typography id="input-minutes" gutterBottom>
+          Minutes per side
+        </Typography>
+        <Slider
+          name="minutes"
+          aria-label="Minutes"
+          defaultValue={5}
+          valueLabelDisplay="auto"
+          step={1}
+          min={1}
+          max={60}
+          onChange={handleMinutesChange}
+        />
+        <Typography id="input-increment" gutterBottom>
+          Increment in seconds
+        </Typography>
+        <Slider
+          name="increment"
+          aria-label="Increment"
+          defaultValue={3}
+          valueLabelDisplay="auto"
+          step={1}
+          min={0}
+          max={60}
+          onChange={handleIncrementChange}
+        />
+        <Grid container justifyContent="center">
+          <ButtonGroup>
+            <IconButton
+              aria-label="white"
+              title="White"
+              onClick={() => handlePlay(Pgn.symbol.WHITE)}
             >
-              <MenuItem key={0} value="rand">
-                Random
-              </MenuItem>
-              <MenuItem key={1} value={Pgn.symbol.WHITE}>
-                White
-              </MenuItem>
-              <MenuItem key={2} value={Pgn.symbol.BLACK}>
-                Black
-              </MenuItem>
-            </TextField>
-            <DialogActions>
-              <Button type="submit">
-                Create Game
-              </Button>
-              <Button onClick={() => dispatch(closePlayOnlineDialog())}>
-                Cancel
-              </Button>
-            </DialogActions>
-          </Paper>
-        </form>
+              <Avatar
+                src={wKing}
+                sx={{ width: 55, height: 55 }}
+              />
+            </IconButton>
+            <IconButton
+              aria-label="random"
+              title="Random"
+              onClick={() => handlePlay('rand')}
+            >
+              <Avatar
+                src={wbKing}
+                sx={{ width: 55, height: 55 }}
+              />
+            </IconButton>
+            <IconButton
+              aria-label="black"
+              title="Black"
+              onClick={() => handlePlay(Pgn.symbol.BLACK)}
+            >
+              <Avatar
+                src={bKing}
+                sx={{ width: 55, height: 55 }}
+              />
+            </IconButton>
+          </ButtonGroup>
+        </Grid>
         <PlayOnlineTable />
       </DialogContent>
     </Dialog>
