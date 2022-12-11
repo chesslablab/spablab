f1f9cec2fc4f7fe21ca76035b16527797350ea66
programarivm <info@programarivm.com>
2022-12-11 17:37:07 +0100

    Displayed image

--- src/features/table/PlayOnlineTable.js
+++ src/features/table/PlayOnlineTable.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import {
+  Avatar,
   Button,
   Paper,
   TableContainer,
@@ -10,6 +11,9 @@ import {
   TableCell,
   TableBody
 } from '@mui/material';
+import wKing from '../../assets/img/pieces/png/150/wKing.png';
+import bKing from '../../assets/img/pieces/png/150/bKing.png';
+import Pgn from '../../common/Pgn';
 import * as mainButtons from '../../features/mainButtonsSlice';
 import * as playOnlineDialog from '../../features/dialog/playOnlineDialogSlice';
 import * as mode from '../../features/mode/modeSlice';
@@ -44,7 +48,13 @@ const PlayOnlineTable = () => {
                 <TableCell align="center">Anonymous</TableCell>
                 <TableCell align="right">{row.min}</TableCell>
                 <TableCell align="right">+{row.increment}</TableCell>
-                <TableCell align="center">{row.color}</TableCell>
+                <TableCell align="center">
+                  {
+                    row.color === Pgn.symbol.WHITE
+                      ? <Avatar src={wKing} sx={{ width: 25, height: 25 }} />
+                      : <Avatar src={bKing} sx={{ width: 25, height: 25 }} />
+                  }
+                </TableCell>
                 <TableCell align="right">
                   <Button
                     disabled={
