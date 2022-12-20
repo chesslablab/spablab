b72ab6628bdcf462a8707ee135a8cb16cf7cf7ae
programarivm <info@programarivm.com>
2022-12-20 11:49:14 +0100

    Removed fade in effect

--- src/features/table/GameTable.js
+++ src/features/table/GameTable.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Fade, Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
 
 const GameTable = ({props}) => {
   const state = useSelector(state => state);
 
   if (state.gameTable.open) {
     return (
-      <Fade in={state.gameTable.open}>
-        <TableContainer sx={{ mt: 1.5 }}>
-          <Table stickyHeader size="small" aria-label="Chess Openings">
-            <TableBody>
-              {
-                Object.entries(state.gameTable.game).map(([key, val]) => (
-                  <TableRow key={key}>
-                    <TableCell align="right">{key}</TableCell>
-                    <TableCell align="left">{val}</TableCell>
-                  </TableRow>
-                ))
-              }
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Fade>
+      <TableContainer sx={{ mt: 1.5 }}>
+        <Table stickyHeader size="small" aria-label="Chess Openings">
+          <TableBody>
+            {
+              Object.entries(state.gameTable.game).map(([key, val]) => (
+                <TableRow key={key}>
+                  <TableCell align="right">{key}</TableCell>
+                  <TableCell align="left">{val}</TableCell>
+                </TableRow>
+              ))
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
     );
   }
 
