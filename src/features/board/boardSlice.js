d01163d4b146e5a707a1f2c0911947fbddc2b930
programarivm <info@programarivm.com>
2023-04-14 10:59:36 +0200

    Replaced state.history with state.fen

--- src/features/board/boardSlice.js
+++ src/features/board/boardSlice.js
@@ -8,7 +8,6 @@ const initialState = {
   isCheck: false,
   isMate: false,
   picked: null,
-  history: [Ascii.toAscii('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')],
   fen: ['rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -'],
   movetext: '',
   flip: Pgn.symbol.WHITE,
@@ -27,7 +26,6 @@ const boardSlice = createSlice({
       const splitFen = action.payload.fen.split(' ');
       state.fen = [action.payload.fen];
       state.turn = splitFen[1];
-      state.history = [Ascii.toAscii(splitFen[0])];
       state.size = {
         files: 10,
         ranks: 8
@@ -37,51 +35,48 @@ const boardSlice = createSlice({
       const splitFen = action.payload.fen.split(' ');
       state.fen = [action.payload.fen];
       state.turn = splitFen[1];
-      state.history = [Ascii.toAscii(splitFen[0])];
     },
     startFen(state, action) {
       const splitFen = action.payload.fen.split(' ');
       state.fen = [action.payload.fen];
       state.turn = splitFen[1];
-      state.history = [Ascii.toAscii(splitFen[0])];
     },
     startPgn(state, action) {
       state.fen = action.payload.fen;
       state.turn = action.payload.turn;
-      state.history = action.payload.history;
       state.movetext = action.payload.movetext;
     },
     pickPiece(state, action) {
+      const splitFen = state.fen[state.fen.length - 1].split(' ')[0];
+      const ascii = Ascii.toAscii(splitFen);
       state.lan = action.payload.sq;
       state.picked = {
         i: action.payload.i,
         j: action.payload.j,
         sq: action.payload.sq,
-        piece: state.history[state.history.length - 1][action.payload.i][action.payload.j]
+        piece: ascii[action.payload.i][action.payload.j]
       };
     },
     leavePiece(state, action) {
       const newTurn = state.turn === Pgn.symbol.WHITE ? Pgn.symbol.BLACK : Pgn.symbol.WHITE;
-      const newAscii = JSON.parse(JSON.stringify(state.history[state.history.length - 1]));
-      const newHistory = JSON.parse(JSON.stringify(state.history));
+      const splitFen = state.fen[state.fen.length - 1].split(' ')[0];
+      const ascii = Ascii.toAscii(splitFen);
       if (state.picked.piece === ' . ') {
         state.picked = null;
       } else if (state.picked.legal_sqs.includes(action.payload.sq)) {
-        newAscii[state.picked.i][state.picked.j] = ' . ';
-        newAscii[action.payload.i][action.payload.j] = state.picked.piece;
+        ascii[state.picked.i][state.picked.j] = ' . ';
+        ascii[action.payload.i][action.payload.j] = state.picked.piece;
         if (state.picked.en_passant) {
           if (action.payload.sq.charAt(0) === state.picked.en_passant.charAt(0)) {
             const index = Ascii.fromAlgebraicToIndex(state.picked.en_passant, state.size);
-            newAscii[index[0]][index[1]] = ' . ';
+            ascii[index[0]][index[1]] = ' . ';
           }
         }
-        newHistory.push(newAscii);
         state.lan += action.payload.sq;
         state.turn = newTurn;
         state.isCheck = false;
         state.isMate = false;
         state.picked = null;
-        state.history = newHistory;
       }
     },
     browseHistory(state) {
@@ -89,29 +84,23 @@ const boardSlice = createSlice({
       state.lan = '';
     },
     castleShort(state, action) {
-      const newHistory = JSON.parse(JSON.stringify(state.history));
       const newFen = JSON.parse(JSON.stringify(state.fen));
-      newHistory[newHistory.length - 1] = Ascii.toAscii(action.payload.fen.split(' ')[0]);
       newFen.push(action.payload.fen);
       state.lan = '';
       state.fen = newFen;
       state.isCheck = action.payload.isCheck;
       state.isMate = action.payload.isMate;
       state.picked = null;
-      state.history = newHistory;
       state.movetext = action.payload.movetext;
     },
     castleLong(state, action) {
-      const newHistory = JSON.parse(JSON.stringify(state.history));
       const newFen = JSON.parse(JSON.stringify(state.fen));
-      newHistory[newHistory.length - 1] = Ascii.toAscii(action.payload.fen.split(' ')[0]);
       newFen.push(action.payload.fen);
       state.lan = '';
       state.fen = newFen;
       state.isCheck = action.payload.isCheck;
       state.isMate = action.payload.isMate;
       state.picked = null;
-      state.history = newHistory;
       state.movetext = action.payload.movetext;
     },
     flip(state) {
@@ -122,9 +111,7 @@ const boardSlice = createSlice({
       state.picked.en_passant = action.payload.en_passant;
     },
     undo(state, action) {
-      const newHistory = JSON.parse(JSON.stringify(state.history));
       const newFen = JSON.parse(JSON.stringify(state.fen));
-      newHistory.splice(-1);
       newFen.splice(-1);
       state.lan = '';
       state.fen = newFen;
@@ -132,32 +119,26 @@ const boardSlice = createSlice({
       state.isCheck = action.payload.isCheck;
       state.isMate = action.payload.isMate;
       state.picked = null;
-      state.history = newHistory;
       state.movetext = action.payload.movetext;
     },
     validMove(state, action) {
-      const newHistory = JSON.parse(JSON.stringify(state.history));
       const newFen = JSON.parse(JSON.stringify(state.fen));
-      newHistory[newHistory.length - 1] = Ascii.toAscii(action.payload.fen.split(' ')[0]);
       newFen.push(action.payload.fen);
       state.lan = '';
       state.fen = newFen;
       state.isCheck = action.payload.isCheck;
       state.isMate = action.payload.isMate;
       state.picked = null;
-      state.history = newHistory;
       state.movetext = action.payload.movetext;
     },
     playMove(state, action) {
-      const newHistory = JSON.parse(JSON.stringify(state.history));
-      newHistory.push(Ascii.toAscii(action.payload.fen.split(' ')[0]));
+      const newFen = JSON.parse(JSON.stringify(state.fen));
+      newFen.push(action.payload.fen);
       state.turn = state.turn === Pgn.symbol.WHITE ? Pgn.symbol.BLACK : Pgn.symbol.WHITE;
-      state.history = newHistory;
+      state.fen = newFen;
     },
     gm(state, action) {
-      const newHistory = JSON.parse(JSON.stringify(state.history));
       const newFen = JSON.parse(JSON.stringify(state.fen));
-      newHistory.push(Ascii.toAscii(action.payload.fen.split(' ')[0]));
       newFen.push(action.payload.fen);
       state.lan = '';
       state.fen = newFen;
@@ -165,7 +146,6 @@ const boardSlice = createSlice({
       state.isCheck = action.payload.isCheck;
       state.isMate = action.payload.isMate;
       state.picked = null;
-      state.history = newHistory;
       state.movetext = action.payload.movetext;
     },
   }
