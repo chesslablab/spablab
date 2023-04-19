c1c07f6f5600aafb1d72acb4664479582af83d3b
programarivm <info@programarivm.com>
2023-04-19 23:38:58 +0200

    Refactored CreateInviteCodeDialog.test.js

--- src/__tests__/features/dialog/CreateInviteCodeDialog.test.js
+++ src/__tests__/features/dialog/CreateInviteCodeDialog.test.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { renderHook } from '@testing-library/react-hooks';
 import store from 'app/store';
 import * as createInviteCodeDialog from 'features/dialog/createInviteCodeDialogSlice';
-import Dispatch from 'test/Dispatch';
-
-const wrapper = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
-);
 
 describe("CreateInviteCodeDialog", () => {
   it("opens the dialog", () => {
-    const actions = [
-      createInviteCodeDialog.open()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.createInviteCodeDialog.open).toBe(true);
+    store.dispatch(createInviteCodeDialog.open());
+    expect(store.getState().createInviteCodeDialog.open).toBe(true);
   });
   it("closes the dialog", () => {
-    const actions = [
-      createInviteCodeDialog.close()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.createInviteCodeDialog.open).toBe(false);
+    store.dispatch(createInviteCodeDialog.close());
+    expect(store.getState().createInviteCodeDialog.open).toBe(false);
   });
 });
