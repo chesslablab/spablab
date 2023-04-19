e235d661419e2336f929bbefd1492366947f8072
programarivm <info@programarivm.com>
2023-04-19 23:37:37 +0200

    Refactored AcceptTakebackDialog.test.js

--- src/__tests__/features/dialog/AcceptTakebackDialog.test.js
+++ src/__tests__/features/dialog/AcceptTakebackDialog.test.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { renderHook } from '@testing-library/react-hooks';
 import store from 'app/store';
 import * as acceptTakebackDialog from 'features/dialog/acceptTakebackDialogSlice';
-import Dispatch from 'test/Dispatch';
-
-const wrapper = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
-);
 
 describe("AcceptTakebackDialog", () => {
   it("opens the dialog", () => {
-    const actions = [
-      acceptTakebackDialog.open()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.acceptTakebackDialog.open).toBe(true);
+    store.dispatch(acceptTakebackDialog.open());
+    expect(store.getState().acceptTakebackDialog.open).toBe(true);
   });
   it("closes the dialog", () => {
-    const actions = [
-      acceptTakebackDialog.close()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.acceptTakebackDialog.open).toBe(false);
+    store.dispatch(acceptTakebackDialog.close());
+    expect(store.getState().acceptTakebackDialog.open).toBe(false);
   });
 });
