273e7e8e2463201c2d0132e5c07bf78fa51c64e3
programarivm <info@programarivm.com>
2023-04-19 23:40:08 +0200

    Refactored LoadFenDialog.test.js

--- src/__tests__/features/dialog/LoadFenDialog.test.js
+++ src/__tests__/features/dialog/LoadFenDialog.test.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { renderHook } from '@testing-library/react-hooks';
 import store from 'app/store';
 import * as loadFenDialog from 'features/dialog/loadFenDialogSlice';
-import Dispatch from 'test/Dispatch';
-
-const wrapper = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
-);
 
 describe("LoadFenDialog", () => {
   it("opens the dialog", () => {
-    const actions = [
-      loadFenDialog.open()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.loadFenDialog.open).toBe(true);
+    store.dispatch(loadFenDialog.open());
+    expect(store.getState().loadFenDialog.open).toBe(true);
   });
   it("closes the dialog", () => {
-    const actions = [
-      loadFenDialog.close()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.loadFenDialog.open).toBe(false);
+    store.dispatch(loadFenDialog.close());
+    expect(store.getState().loadFenDialog.open).toBe(false);
   });
 });
