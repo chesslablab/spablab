30e7d9430ceadd453cc3c6d41fac6baf59b3c5d4
programarivm <info@programarivm.com>
2023-04-19 23:41:27 +0200

    Refactored LoadPgnDialog.test.js

--- src/__tests__/features/dialog/LoadPgnDialog.test.js
+++ src/__tests__/features/dialog/LoadPgnDialog.test.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { renderHook } from '@testing-library/react-hooks';
 import store from 'app/store';
 import * as loadPgnDialog from 'features/dialog/loadPgnDialogSlice';
-import Dispatch from 'test/Dispatch';
-
-const wrapper = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
-);
 
 describe("LoadPgnDialog", () => {
   it("opens the dialog", () => {
-    const actions = [
-      loadPgnDialog.open()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.loadPgnDialog.open).toBe(true);
+    store.dispatch(loadPgnDialog.open());
+    expect(store.getState().loadPgnDialog.open).toBe(true);
   });
   it("closes the dialog", () => {
-    const actions = [
-      loadPgnDialog.close()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.loadPgnDialog.open).toBe(false);
+    store.dispatch(loadPgnDialog.close());
+    expect(store.getState().loadPgnDialog.open).toBe(false);
   });
 });
