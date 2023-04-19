d15de1a5183396deecd5f83e63af4bfd29d3281b
programarivm <info@programarivm.com>
2023-04-19 23:43:40 +0200

    Refactored OfferTakebackDialog.test.js

--- src/__tests__/features/dialog/OfferTakebackDialog.test.js
+++ src/__tests__/features/dialog/OfferTakebackDialog.test.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { renderHook } from '@testing-library/react-hooks';
 import store from 'app/store';
 import * as offerTakebackDialog from 'features/dialog/offerTakebackDialogSlice';
-import Dispatch from 'test/Dispatch';
-
-const wrapper = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
-);
 
 describe("OfferTakebackDialog", () => {
   it("opens the dialog", () => {
-    const actions = [
-      offerTakebackDialog.open()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.offerTakebackDialog.open).toBe(true);
+    store.dispatch(offerTakebackDialog.open());
+    expect(store.getState().offerTakebackDialog.open).toBe(true);
   });
   it("closes the dialog", () => {
-    const actions = [
-      offerTakebackDialog.close()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.offerTakebackDialog.open).toBe(false);
+    store.dispatch(offerTakebackDialog.close());
+    expect(store.getState().offerTakebackDialog.open).toBe(false);
   });
 });
