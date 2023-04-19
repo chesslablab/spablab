c05c9a22c27fded8d0fc1c228d69376a3511f601
programarivm <info@programarivm.com>
2023-04-19 23:33:39 +0200

    Refactored InfoAlert.test.js

--- src/__tests__/features/alert/InfoAlert.test.js
+++ src/__tests__/features/alert/InfoAlert.test.js
@@ -1,36 +1,20 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { renderHook } from '@testing-library/react-hooks';
 import store from 'app/store';
 import * as infoAlert from 'features/alert/infoAlertSlice';
-import Dispatch from 'test/Dispatch';
-
-const wrapper = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
-);
 
 describe("InfoAlert", () => {
   it("opens the alert", () => {
-    const actions = [
-      infoAlert.show({ info: 'This is an informative message.' })
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.infoAlert.open).toBe(true);
-    expect(result.current.state.infoAlert.info).toBe('This is an informative message.');
+    store.dispatch(infoAlert.show({ info: 'This is an informative message.' }));
+    expect(store.getState().infoAlert.open).toBe(true);
+    expect(store.getState().infoAlert.info).toBe('This is an informative message.');
   });
   it("closes the alert", () => {
-    const actions = [
-      infoAlert.close()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.infoAlert.open).toBe(false);
+    store.dispatch(infoAlert.close());
+    expect(store.getState().infoAlert.open).toBe(false);
   });
   it("opens and closes the alert", () => {
-    const actions = [
-      infoAlert.show({ info: 'This is an informative message.' }),
-      infoAlert.close()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.infoAlert.open).toBe(false);
+    store.dispatch(infoAlert.show({ info: 'This is an informative message.' }));
+    store.dispatch(infoAlert.close());
+    expect(store.getState().infoAlert.open).toBe(false);
   });
 });
