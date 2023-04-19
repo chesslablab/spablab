108da41b8e32246c4628d05b19eb119be564ac4e
programarivm <info@programarivm.com>
2023-04-19 23:42:28 +0200

    Refactored OfferDrawDialog.test.js

--- src/__tests__/features/dialog/OfferDrawDialog.test.js
+++ src/__tests__/features/dialog/OfferDrawDialog.test.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { renderHook } from '@testing-library/react-hooks';
 import store from 'app/store';
 import * as offerDrawDialog from 'features/dialog/offerDrawDialogSlice';
-import Dispatch from 'test/Dispatch';
-
-const wrapper = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
-);
 
 describe("OfferDrawDialog", () => {
   it("opens the dialog", () => {
-    const actions = [
-      offerDrawDialog.open()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.offerDrawDialog.open).toBe(true);
+    store.dispatch(offerDrawDialog.open());
+    expect(store.getState().offerDrawDialog.open).toBe(true);
   });
   it("closes the dialog", () => {
-    const actions = [
-      offerDrawDialog.close()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.offerDrawDialog.open).toBe(false);
+    store.dispatch(offerDrawDialog.close());
+    expect(store.getState().offerDrawDialog.open).toBe(false);
   });
 });
