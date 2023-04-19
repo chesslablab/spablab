fd09433fcd5d453802d6cb32f53f1b06d4247811
programarivm <info@programarivm.com>
2023-04-19 23:36:06 +0200

    Refactored AcceptDrawDialog.test.js

--- src/__tests__/features/dialog/AcceptDrawDialog.test.js
+++ src/__tests__/features/dialog/AcceptDrawDialog.test.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { renderHook } from '@testing-library/react-hooks';
 import store from 'app/store';
 import * as acceptDrawDialog from 'features/dialog/acceptDrawDialogSlice';
-import Dispatch from 'test/Dispatch';
-
-const wrapper = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
-);
 
 describe("AcceptDrawDialog", () => {
   it("opens the dialog", () => {
-    const actions = [
-      acceptDrawDialog.open()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.acceptDrawDialog.open).toBe(true);
+    store.dispatch(acceptDrawDialog.open());
+    expect(store.getState().acceptDrawDialog.open).toBe(true);
   });
   it("closes the dialog", () => {
-    const actions = [
-      acceptDrawDialog.close()
-    ];
-    const { result } = renderHook(() => Dispatch(actions), { wrapper });
-    expect(result.current.state.acceptDrawDialog.open).toBe(false);
+    store.dispatch(acceptDrawDialog.close());
+    expect(store.getState().acceptDrawDialog.open).toBe(false);
   });
 });
