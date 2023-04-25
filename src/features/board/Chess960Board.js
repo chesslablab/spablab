82d427077008d7712f9ed6430041e5c9edf96fca
programarivm <info@programarivm.com>
2023-04-25 11:35:02 +0200

    Implemented animation in Chess960Board

--- src/features/board/Chess960Board.js
+++ src/features/board/Chess960Board.js
@@ -1,5 +1,7 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useMediaQuery } from '@mui/material';
+import Animation from 'common/Animation';
 import Piece from 'common/Piece';
 import * as board from 'features/board/boardSlice';
 import Squares from 'features/board/Squares';
@@ -8,13 +10,32 @@ import WsAction from 'features/ws/WsAction';
 const Chess960Board = ({props}) => {
   const state = useSelector(state => state);
   const dispatch = useDispatch();
+  const isInitialMount = useRef(true);
+  const maxWidth = {
+    '600': useMediaQuery("(max-width:600px)"),
+    '900': useMediaQuery("(max-width:900px)")
+  };
+  const sqSize = maxWidth['600'] ? 12 : maxWidth['900'] ? 10 : 4.1;
   const sqsRef = useRef([]);
   const imgsRef = useRef([]);
+
+  useEffect(() => {
+    if (isInitialMount.name) {
+      isInitialMount.name = false;
+    } else {
+      if (state.board.fen.length > 1) {
+        new Animation(sqSize, imgsRef, sqsRef).pieces();
+      }
+    }
+  }, [
+    state.board.fen.length,
+    sqSize
+  ]);
 
   const handleMove = (payload) => {
     if (state.board.turn === Piece.color(payload.piece)) {
       // allow the king to be dropped into the castling rook
-      if (state.board.picked) {
+      if (state.board.picked?.fen) {
         if (Object.keys(state.board.picked.fen).includes(payload.sq)) {
           dispatch(board.leavePiece(payload));
         } else {
