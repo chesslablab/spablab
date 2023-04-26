e5870c5f6355eadb1f5b839f2598d22fb9fac18c
programarivm <info@programarivm.com>
2023-04-26 17:54:06 +0200

    Fixed animation

--- src/common/Animation.js
+++ src/common/Animation.js
@@ -16,50 +16,52 @@ export default class Animation {
         store.getState().board?.piecePlaced?.event === eventConst.ON_STOCKFISH ||
         store.getState().board?.piecePlaced?.event === eventConst.ON_PLAY_LAN
       ) {
-        const fromFen = store.getState().board.fen[store.getState().board.fen.length - 2].split(' ');
-        const toFen = store.getState().board.fen[store.getState().board.fen.length - 1].split(' ');
+        if (!store.getState().board.isMate) {
+          const fromFen = store.getState().board.fen[store.getState().board.fen.length - 2].split(' ');
+          const toFen = store.getState().board.fen[store.getState().board.fen.length - 1].split(' ');
 
-        const lan = Ascii.longAlgebraicNotation(
-          Ascii.toAscii(fromFen[0]),
-          Ascii.toAscii(toFen[0])
-        );
+          const lan = Ascii.longAlgebraicNotation(
+            Ascii.toAscii(fromFen[0]),
+            Ascii.toAscii(toFen[0])
+          );
 
-        const sqDiff = Ascii.sqDiff(lan[0], lan[1]);
+          const sqDiff = Ascii.sqDiff(lan[0], lan[1]);
 
-        const xAxis = Ascii.xAxisSign(
-          lan[0],
-          lan[1],
-          store.getState().board.turn,
-          store.getState().board.flip
-        ) * this.sqSize * sqDiff.files;
+          const xAxis = Ascii.xAxisSign(
+            lan[0],
+            lan[1],
+            store.getState().board.turn,
+            store.getState().board.flip
+          ) * this.sqSize * sqDiff.files;
 
-        const yAxis = Ascii.yAxisSign(
-          lan[0],
-          lan[1],
-          store.getState().board.turn,
-          store.getState().board.flip
-        ) * this.sqSize * sqDiff.ranks;
+          const yAxis = Ascii.yAxisSign(
+            lan[0],
+            lan[1],
+            store.getState().board.turn,
+            store.getState().board.flip
+          ) * this.sqSize * sqDiff.ranks;
 
-        const sqFrom = this.sqsRef.current[lan[0]];
-        const sqTo = this.sqsRef.current[lan[1]];
-        const img = this.imgsRef.current[lan[1]];
+          const sqFrom = this.sqsRef.current[lan[0]];
+          const sqTo = this.sqsRef.current[lan[1]];
+          const img = this.imgsRef.current[lan[1]];
 
-        sqFrom.appendChild(img);
+          sqFrom.appendChild(img);
 
-        img.animate(
-          {
-            transform: `translate(${xAxis}vw, ${yAxis}vw)`
-          },
-          {
-            duration: 100
-          }
-        );
+          img.animate(
+            {
+              transform: `translate(${xAxis}vw, ${yAxis}vw)`
+            },
+            {
+              duration: 100
+            }
+          );
 
-        Promise.all(
-          img.getAnimations().map((animation) => animation.finished),
-        ).then(() => {
-          sqTo.appendChild(img);
-        });
+          Promise.all(
+            img.getAnimations().map((animation) => animation.finished),
+          ).then(() => {
+            sqTo.appendChild(img);
+          });
+        }
       }
     }
   }
