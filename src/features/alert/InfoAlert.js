73bfd8de2791162617c76d410f57e45b5a9f6a52
programarivm <info@programarivm.com>
2023-05-15 15:29:01 +0200

    Refactored InfoAlert

--- src/features/alert/InfoAlert.js
+++ src/features/alert/InfoAlert.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Alert } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { Alert, Backdrop, Button } from '@mui/material';
+import * as infoAlert from 'features/alert/infoAlertSlice';
 
 const InfoAlert = () => {
   const state = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const handleClose = () => {
+    dispatch(infoAlert.close());
+  };
 
   if (state.infoAlert.open) {
     return (
-      <Alert sx={{ mt: 1.5 }} severity="info">
-        {state.infoAlert.info}
-      </Alert>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={state.infoAlert.open}
+      >
+        <Alert
+          severity="info"
+          action={
+            <Button color="inherit" size="small" onClick={handleClose}>
+              OK
+            </Button>
+          }
+        >
+          {state.infoAlert.info}
+        </Alert>
+      </Backdrop>
     );
   }
 
