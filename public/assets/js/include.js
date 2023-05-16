811797c24afc7922cd13a36393ad9b866528d092
programarivm <info@programarivm.com>
2023-05-16 17:21:13 +0200

    Added footer

--- public/assets/js/include.js
+++ public/assets/js/include.js
@@ -5,3 +5,11 @@ fetch("../nav.html")
   .then(data => {
     document.querySelector("nav").innerHTML = data;
   });
+
+fetch("../footer.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("footer").innerHTML = data;
+  });
