4dc9c61e322788167e6530e8db41c22273af3fae
programarivm <info@programarivm.com>
2023-05-16 10:20:57 +0200

    Added divider

--- src/features/table/PlayOnlineTable.js
+++ src/features/table/PlayOnlineTable.js
@@ -5,6 +5,7 @@ import RestartAltIcon from '@mui/icons-material/RestartAlt';
 import ShuffleIcon from '@mui/icons-material/Shuffle';
 import {
   Avatar,
+  Divider,
   Paper,
   TableContainer,
   Table,
@@ -64,47 +65,50 @@ const PlayOnlineTable = () => {
 
   if (state.playOnlineDialog.rows.length > 0) {
     return (
-      <TableContainer component={Paper} sx={{ mb: 3 }}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center" colSpan={5}>Online Invitations</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {state.playOnlineDialog.rows.map((row, i) => (
-              <TableRow
-                key={i}
-                selected={true}
-                sx={
-                  state.mode.play && state.mode.play.hash === row.hash
-                    ? styles.disabled
-                    : styles.clickable
-                }
-                onClick={() =>
-                  state.mode.play && state.mode.play.hash === row.hash
-                    ? null
-                    : handlePlay(row.hash)
-                }
-              >
-                <TableCell align="center">Anonymous</TableCell>
-                <TableCell align="center">{row.min}</TableCell>
-                <TableCell align="center">+{row.increment}</TableCell>
-                <TableCell align="center">
-                  {
-                    row.color === Pgn.symbol.WHITE
-                      ? <Avatar src={wKing} sx={{ mt: -0.85, width: 25, height: 25 }} />
-                      : <Avatar src={bKing} sx={{ mt: -0.85, width: 25, height: 25 }} />
-                  }
-                </TableCell>
-                <TableCell align="center">
-                  <VariantIcon props={{ variant: row.variant }} />
-                </TableCell>
+      <>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center" colSpan={5}>Invitations</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {state.playOnlineDialog.rows.map((row, i) => (
+                <TableRow
+                  key={i}
+                  selected={true}
+                  sx={
+                    state.mode.play && state.mode.play.hash === row.hash
+                      ? styles.disabled
+                      : styles.clickable
+                  }
+                  onClick={() =>
+                    state.mode.play && state.mode.play.hash === row.hash
+                      ? null
+                      : handlePlay(row.hash)
+                  }
+                >
+                  <TableCell align="center">Anonymous</TableCell>
+                  <TableCell align="center">{row.min}</TableCell>
+                  <TableCell align="center">+{row.increment}</TableCell>
+                  <TableCell align="center">
+                    {
+                      row.color === Pgn.symbol.WHITE
+                        ? <Avatar src={wKing} sx={{ mt: -0.85, width: 25, height: 25 }} />
+                        : <Avatar src={bKing} sx={{ mt: -0.85, width: 25, height: 25 }} />
+                    }
+                  </TableCell>
+                  <TableCell align="center">
+                    <VariantIcon props={{ variant: row.variant }} />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Divider sx={{ mt: 3, mb: 3 }} />
+      </>
     );
   }
 
