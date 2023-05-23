208ba1fa7200beded75be4803298bec2ae943df1
programarivm <info@programarivm.com>
2023-05-23 13:25:12 +0200

    Refactored the mode dialogs

--- src/features/nav/mainButtonsSlice.js
+++ src/features/nav/mainButtonsSlice.js
@@ -3,6 +3,49 @@ import * as mainButtonsConst from 'features/mainButtonsConst';
 
 const initialState = {
   name: mainButtonsConst.ANALYSIS,
+  dialogs: {
+    createInboxCode: {
+      open: false,
+      inbox: {},
+    },
+    enterInboxCode: {
+      open: false,
+      inbox: {},
+    },
+    eventsStats: {
+      open: false,
+      autocomplete: {
+        events: [],
+        players: [],
+      },
+    },
+    heuristics: {
+      open: false,
+      heuristics: null,
+    },
+    openingsStats: {
+      open: false,
+      stats: {
+        drawRate: [],
+        winRateForWhite: [],
+        winRateForBlack: [],
+      },
+    },
+    playersStats: {
+      open: false,
+      autocomplete: {
+        events: [],
+        players: [],
+      },
+    },
+    settings: {
+      open: false,
+      fields: {
+        pieceAnimation: 'on',
+        heuristics: 'off',
+      },
+    },
+  },
 };
 
 const mainButtonsSlice = createSlice({
@@ -21,8 +64,29 @@ const mainButtonsSlice = createSlice({
     },
     setDatabase: (state) => {
       state.name = mainButtonsConst.DATABASE;
-    }
-  }
+    },
+    createInboxCodeDialog(state, action) {
+      state.dialogs.createInboxCode = action.payload;
+    },
+    enterInboxCodeDialog(state, action) {
+      state.dialogs.enterInboxCode = action.payload;
+    },
+    eventsStatsDialog(state, action) {
+      state.dialogs.eventsStats = action.payload;
+    },
+    heuristicsDialog(state, action) {
+      state.dialogs.heuristics = action.payload;
+    },
+    openingsStatsDialog(state, action) {
+      state.dialogs.openingsStats = action.payload;
+    },
+    playersStatsDialog(state, action) {
+      state.dialogs.playersStats = action.payload;
+    },
+    settingsDialog(state, action) {
+      state.dialogs.settings = action.payload;
+    },
+  },
 });
 
 export const {
@@ -30,6 +94,13 @@ export const {
   setPlay,
   setTraining,
   setOpeningSearch,
-  setDatabase
+  setDatabase,
+  createInboxCodeDialog,
+  enterInboxCodeDialog,
+  eventsStatsDialog,
+  heuristicsDialog,
+  openingsStatsDialog,
+  playersStatsDialog,
+  settingsDialog
 } = mainButtonsSlice.actions;
 export default mainButtonsSlice.reducer;
