f57381a5d6400807bf532ea70e41fec68b3cc69b
programarivm <info@programarivm.com>
2023-05-29 10:24:19 +0200

    Fixed OpeningTable

--- src/features/panel/OpeningTable.js
+++ src/features/panel/OpeningTable.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
+import * as navConst from 'features/nav/navConst';
 
 const OpeningTable = ({props}) => {
   const state = useSelector(state => state);
 
-  if (state.panel.tables.opening.rows) {
-    return (
-      <TableContainer sx={{ mt: 1.5 }}>
-        <Table stickyHeader size="small" aria-label="Chess Openings">
-          <TableBody>
-            {
-              state.panel.tables.opening.rows.map((item, i) => (
-                <TableRow key={i}>
-                  <TableCell align="left">{item.eco}</TableCell>
-                  <TableCell align="left">{item.name}</TableCell>
-                </TableRow>
-              ))
-            }
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
+  if (state.nav?.name !== navConst.DATABASE && state.nav?.name !== navConst.TRAINING) {
+    if (state.panel.tables.opening.rows) {
+      return (
+        <TableContainer sx={{ mt: 1.5 }}>
+          <Table stickyHeader size="small" aria-label="Chess Openings">
+            <TableBody>
+              {
+                state.panel.tables.opening.rows.map((item, i) => (
+                  <TableRow key={i}>
+                    <TableCell align="left">{item.eco}</TableCell>
+                    <TableCell align="left">{item.name}</TableCell>
+                  </TableRow>
+                ))
+              }
+            </TableBody>
+          </Table>
+        </TableContainer>
+      );
+    }
   }
 
   return null;
