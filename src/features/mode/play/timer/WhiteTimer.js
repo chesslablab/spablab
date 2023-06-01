ad2a6e44c695b869d2b212a7eafd41536464c3d2
programarivm <info@programarivm.com>
2023-06-01 20:58:39 +0200

    Refactored WhiteTimer

--- src/features/mode/play/timer/WhiteTimer.js
+++ src/features/mode/play/timer/WhiteTimer.js
@@ -9,42 +9,25 @@ const WhiteTimer = () => {
   const state = useSelector(state => state);
   const dispatch = useDispatch();
 
-  const now = new Date();
-  const [expiration, setExpiration] = useState(now.setSeconds(now.getSeconds() + state.playMode.timer.w));
-
-  const calculateTimeLeft = () => {
-    const diff = expiration - new Date();
-    let timeLeft = {};
-
-    if (diff > 0) {
-      timeLeft = {
-        hours: Math.floor(diff / (1000 * 60 * 60)),
-        minutes: Math.floor((diff / 1000 / 60) % 60),
-        seconds: Math.floor((diff / 1000) % 60),
-      };
-    }
-
-    return timeLeft;
-  };
-
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [count, setCount] = useState(state.playMode.timer.w);
 
   useEffect(() => {
     setTimeout(() => {
       if (state.board.turn === Pgn.symbol.WHITE) {
-        setTimeLeft(calculateTimeLeft());
-      } else {
-        const now = new Date();
-        now.setSeconds(now.getSeconds() + state.playMode.timer.w)
-        setExpiration(now);
-        setTimeLeft(calculateTimeLeft());
+        setCount(prevCount => prevCount - 1);
       }
     }, 1000);
   });
 
+  useEffect(() => {
+    setCount(state.playMode.timer.w);
+  }, [
+    state.playMode.timer.w
+  ]);
+
   return (
     <Box component="span" style={{ marginRight: 10 }}>
-      {timeLeft.minutes}:{timeLeft.seconds}
+      {Math.floor(count / (60 * 60))}:{Math.floor((count / 60) % 60)}:{Math.floor(count % 60)}
     </Box>
   );
 }
