abbbd537ea68b2aafb67dd01b42103daf058e3e5
programarivm <info@programarivm.com>
2023-06-04 12:41:23 +0200

    Refactored FinishedButtonsPlayMode

--- src/features/mode/play/FinishedButtonsPlayMode.js
+++ src/features/mode/play/FinishedButtonsPlayMode.js
@@ -9,32 +9,31 @@ const FinishedButtonsPlayMode = () => {
   const state = useSelector(state => state);
   const dispatch = useDispatch();
 
+  const disabled = state.board.isMate ||
+    state.board.isStalemate ||
+    state.playMode.draw === Wording.verb.ACCEPT.toLowerCase() ||
+    state.playMode.resign === Wording.verb.ACCEPT.toLowerCase() ||
+    state.playMode.timeOut;
+
   if (state.playMode.active) {
     if (state.playMode.accepted) {
-      if (
-        state.board.isMate ||
-        state.board.isStalemate ||
-        state.playMode.draw === Wording.verb.ACCEPT.toLowerCase() ||
-        state.playMode.resign === Wording.verb.ACCEPT.toLowerCase() ||
-        state.playMode.timeOut
-      ) {
-        return (
-          <ButtonGroup
-            sx={{ mt: 1.5 }}
-            orientation="vertical"
-            size="small"
-            aria-label="Game Over"
-            fullWidth={true}
-          >
-            <Button onClick={() => {
-              Ws.rematch(Wording.verb.PROPOSE.toLowerCase());
-              dispatch(playMode.proposeRematch());
-            }}>
-              Offer Rematch
-            </Button>
-          </ButtonGroup>
-        );
-      }
+      return (
+        <ButtonGroup
+          sx={{ mt: 1.5 }}
+          orientation="vertical"
+          size="small"
+          aria-label="Game Over"
+          fullWidth={true}
+          disabled={disabled}
+        >
+          <Button onClick={() => {
+            Ws.rematch(Wording.verb.PROPOSE.toLowerCase());
+            dispatch(playMode.proposeRematch());
+          }}>
+            Offer Rematch
+          </Button>
+        </ButtonGroup>
+      );
     }
   }
 
