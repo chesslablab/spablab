db2114111856d078cc7c80ff439cce0728c43494
programarivm <info@programarivm.com>
2023-07-27 18:36:19 +0200

    Added AnnotatedGamesTable

--- src/features/mode/san/dialog/AnnotatedGamesDialog.js
+++ src/features/mode/san/dialog/AnnotatedGamesDialog.js
@@ -1,27 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CloseIcon from '@mui/icons-material/Close';
 import {
-  Button,
   Dialog,
   DialogContent,
   DialogTitle,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText
+  IconButton
 } from '@mui/material';
 import * as sanMode from 'features/mode/sanModeSlice';
+import AnnotatedGamesTable from 'features/mode/san/table/AnnotatedGamesTable.js';
 
 const AnnotatedGamesDialog = ({props}) => {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
+  const [result, setResult] = useState([]);
 
-  const handleRead = async (event) => {
-    event.preventDefault();
-    // TODO ...
-  };
+  const annotatedGames = require('assets/json/annotated-games.json');
+
+  useEffect(() => {
+    setResult(annotatedGames);
+  }, [annotatedGames]);
 
   return (
     <Dialog open={state.sanMode.dialogs.annotatedGames.open} maxWidth="md" fullWidth={true}>
@@ -32,28 +30,7 @@ const AnnotatedGamesDialog = ({props}) => {
         </IconButton>
       </DialogTitle>
       <DialogContent>
-        <form onSubmit={handleRead}>
-          <List>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemText primary="Trash" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton component="a" href="#simple-list">
-                <ListItemText primary="Spam" />
-              </ListItemButton>
-            </ListItem>
-          </List>
-          <Button
-            fullWidth
-            variant="outlined"
-            type="submit"
-            sx={{ mt: 2 }}
-          >
-            Read
-          </Button>
-        </form>
+        <AnnotatedGamesTable props={{ result: result }} />
       </DialogContent>
     </Dialog>
   );
