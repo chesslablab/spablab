936bbaa92b3c8d317dd83654f72c23a19bf301a5
programarivm <info@programarivm.com>
2023-07-30 22:57:07 +0200

    Refactored panel

--- src/features/mode/fen/panel/FenPanel.js
+++ src/features/mode/fen/panel/FenPanel.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Grid } from '@mui/material';
-import StartedButtons from 'features/panel/play/StartedButtons';
-import StartedDialogs from 'features/panel/play/StartedDialogs';
-import FinishedButtons from 'features/panel/play/FinishedButtons';
-import FinishedDialogs from 'features/panel/play/FinishedDialogs';
-import Timer from 'features/panel/play/timer/Timer';
+import StartedButtons from 'features/mode/fen/panel/StartedButtons';
 import Buttons from 'features/panel/Buttons';
 import ButtonsDialogs from 'features/panel/ButtonsDialogs';
 import History from 'features/panel/History';
@@ -12,7 +8,7 @@ import MovesTable from 'features/panel/MovesTable';
 import OpeningTable from 'features/panel/OpeningTable';
 import styles from 'features/panel/styles';
 
-const PlayPanel = ({ props }) => {
+const FenPanel = ({ props }) => {
   return (
     <Grid container>
       <Grid item xs={12} sx={styles.gameBox}>
@@ -28,19 +24,13 @@ const PlayPanel = ({ props }) => {
           <Buttons props={props} />
         </Grid>
       </Grid>
-      <Grid item xs={12} sx={styles.timer}>
-        <Timer />
-      </Grid>
       <Grid item xs={12}>
         <StartedButtons />
-        <FinishedButtons />
         <OpeningTable />
       </Grid>
       <ButtonsDialogs />
-      <StartedDialogs />
-      <FinishedDialogs />
     </Grid>
   );
 };
 
-export default PlayPanel;
+export default FenPanel;
