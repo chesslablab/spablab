6639b88af782d4a32cbbde4b05ae1297f9fefc01
programarivm <info@programarivm.com>
2023-08-04 12:17:41 +0200

    Refactored EventAutocomplete

--- src/features/autocomplete/EventAutocomplete.js
+++ src/features/autocomplete/EventAutocomplete.js
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { TextField } from '@mui/material';
 import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import * as eventAutocomplete from 'features/autocomplete/eventAutocompleteSlice';
 import * as warningAlert from 'features/alert/warningAlertSlice';
-import * as progressDialog from 'features/progressDialogSlice';
 
 const filterOptions = createFilterOptions({
   matchFrom: 'any',
@@ -15,34 +14,31 @@ const EventAutocomplete = ({props}) => {
   const state = useSelector(state => state);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (state.eventAutocomplete.data.length === 0) {
-      fetch(`${props.api.prot}://${props.api.host}:${props.api.port}/api/autocomplete/event`)
-        .then(res => {
-          if (res.status === 200) {
-            res.json().then(data => {
-              dispatch(eventAutocomplete.set(data));
-            });
-          } else {
-            dispatch(warningAlert.show({ mssg: 'Whoops! Something went wrong, please try again.' }));
-          }
-        })
-        .finally(() => {
-          dispatch(progressDialog.close());
-        });
-    }
-  }, [
-    state.eventAutocomplete.data.length,
-    props,
-    dispatch
-  ]);
-
   return (
     <Autocomplete
       id="Event"
       options={state.eventAutocomplete.data}
       filterOptions={filterOptions}
-      renderInput={(params) => <TextField {...params} label="Event" variant="filled" name="Event" />}
+      renderInput={(params) =>
+        <TextField {...params}
+          label="Event"
+          variant="filled"
+          name="Event"
+          onClick={() => {
+            if (state.eventAutocomplete.data.length === 0) {
+              fetch(`${props.api.prot}://${props.api.host}:${props.api.port}/api/autocomplete/event`)
+                .then(res => {
+                  if (res.status === 200) {
+                    res.json().then(data => {
+                      dispatch(eventAutocomplete.set(data));
+                    });
+                  } else {
+                    dispatch(warningAlert.show({ mssg: 'Whoops! Something went wrong, please try again.' }));
+                  }
+                });
+            }
+          }}
+        />}
     />
   );
 };
