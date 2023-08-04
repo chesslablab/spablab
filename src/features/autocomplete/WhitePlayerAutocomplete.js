4ef7d9eb6e4b8162ecd2aa683170ff0f3c6b1ebf
programarivm <info@programarivm.com>
2023-08-04 12:20:43 +0200

    Refactored WhitePlayerAutocomplete

--- src/features/autocomplete/WhitePlayerAutocomplete.js
+++ src/features/autocomplete/WhitePlayerAutocomplete.js
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { TextField } from '@mui/material';
 import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import * as playerAutocomplete from 'features/autocomplete/playerAutocompleteSlice';
 import * as warningAlert from 'features/alert/warningAlertSlice';
-import * as progressDialog from 'features/progressDialogSlice';
 
 const filterOptions = createFilterOptions({
   matchFrom: 'any',
@@ -15,34 +14,31 @@ const WhitePlayerAutocomplete = ({props}) => {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (state.playerAutocomplete.data.length === 0) {
-      fetch(`${props.api.prot}://${props.api.host}:${props.api.port}/api/autocomplete/player`)
-        .then(res => {
-          if (res.status === 200) {
-            res.json().then(data => {
-              dispatch(playerAutocomplete.set(data));
-            });
-          } else {
-            dispatch(warningAlert.show({ mssg: 'Whoops! Something went wrong, please try again.' }));
-          }
-        })
-        .finally(() => {
-          dispatch(progressDialog.close());
-        });
-    }
-  }, [
-    state.playerAutocomplete.data.length,
-    props,
-    dispatch
-  ]);
-
   return (
     <Autocomplete
       id="White"
       options={state.playerAutocomplete.data}
       filterOptions={filterOptions}
-      renderInput={(params) => <TextField {...params} label="White" variant="filled" name="White" />}
+      renderInput={(params) =>
+        <TextField {...params}
+          label="White"
+          variant="filled"
+          name="White"
+          onClick={() => {
+            if (state.eventAutocomplete.data.length === 0) {
+              fetch(`${props.api.prot}://${props.api.host}:${props.api.port}/api/autocomplete/player`)
+                .then(res => {
+                  if (res.status === 200) {
+                    res.json().then(data => {
+                      dispatch(playerAutocomplete.set(data));
+                    });
+                  } else {
+                    dispatch(warningAlert.show({ mssg: 'Whoops! Something went wrong, please try again.' }));
+                  }
+                });
+            }
+          }}
+        />}
     />
   );
 };
