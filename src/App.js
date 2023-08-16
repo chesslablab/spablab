63e394222f0554fbd6e65d837d4760551c4fa1de
programarivm <info@programarivm.com>
2023-08-16 11:42:10 +0200

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,7 @@ import Chess from 'features/Chess';
 
 const App = () => {
   return (
-    <Chess
-      props={{
-        api: {
-          prot: 'https',
-          host: 'pchess.net',
-          port: '443'
-        },
-        ws: {
-          prot: 'wss',
-          host: 'pchess.net',
-          port: '8443'
-        }
-      }}
-    />
+    <Chess/>
   );
 }
 
