5094b8a0481c7808be3a823d7933eb0d60b9ffaf
programarivm <info@programarivm.com>
2023-08-20 13:16:30 +0200

    Fixed OpeningTable

--- src/features/panel/OpeningTable.js
+++ src/features/panel/OpeningTable.js
@@ -1,28 +1,30 @@
 import { useSelector } from 'react-redux';
 import { Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
-import * as navConst from 'features/nav/navConst';
+import Ascii from 'common/Ascii';
 
 const OpeningTable = () => {
   const state = useSelector(state => state);
 
-  if (state.nav?.name !== navConst.DATABASE && state.nav?.name !== navConst.TRAINING) {
-    if (state.panel.tables.opening.rows) {
-      return (
-        <TableContainer sx={{ mt: 1.5 }}>
-          <Table stickyHeader size="small" aria-label="Chess Openings">
-            <TableBody>
-              {
-                state.panel.tables.opening.rows.map((item, i) => (
-                  <TableRow key={i}>
-                    <TableCell align="left">{item.eco}</TableCell>
-                    <TableCell align="left">{item.name}</TableCell>
-                  </TableRow>
-                ))
-              }
-            </TableBody>
-          </Table>
-        </TableContainer>
-      );
+  if (state.fenMode.active || state.sanMode.active || state.stockfishMode.active) {
+    if (state.board.fen[0] === Ascii.initialFen()) {
+      if (state.panel.tables.opening.rows) {
+        return (
+          <TableContainer sx={{ mt: 1.5 }}>
+            <Table stickyHeader size="small" aria-label="Chess Openings">
+              <TableBody>
+                {
+                  state.panel.tables.opening.rows.map((item, i) => (
+                    <TableRow key={i}>
+                      <TableCell align="left">{item.eco}</TableCell>
+                      <TableCell align="left">{item.name}</TableCell>
+                    </TableRow>
+                  ))
+                }
+              </TableBody>
+            </Table>
+          </TableContainer>
+        );
+      }
     }
   }
 
