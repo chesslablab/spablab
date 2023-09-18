d0ad4db4f3588f3bbc9c44ab9aec42595478f222
Shaikh Faisal Husain <faisalhs2002@gmail.com>
2023-09-18 13:19:05 +0530

    slider added for changing chessboard size

---
+++ src/features/board/BoardSlider.js
@@ -0,0 +1,33 @@
+import Slider from "@mui/material/Slider";
+import { useRef, useState } from "react";
+
+const BoardSlider = () => {
+  const [sliderValue, setSliderValue] = useState(100);
+
+  const sliderRef = useRef(document.documentElement);
+
+  const handleChange = (e, val) => {
+    console.log(sliderValue);
+    setSliderValue(val);
+    console.log(val);
+    sliderRef.current.style.setProperty("--slider-value", val);
+  };
+
+  return (
+    <>
+      <Slider
+        aria-label="Volume"
+        value={sliderValue}
+        onChange={handleChange}
+        min={30}
+        max={100}
+        className="slider"
+        sx={{
+            marginTop: '10px'
+        }}
+      />
+    </>
+  );
+};
+
+export default BoardSlider;
