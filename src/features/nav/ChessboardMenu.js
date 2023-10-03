1edff84cb4a90db198e77650fd0e9862958c0ec9
programarivm <info@programarivm.com>
2023-10-03 12:23:25 +0200

    Wording

--- src/features/nav/ChessboardMenu.js
+++ src/features/nav/ChessboardMenu.js
@@ -59,7 +59,7 @@ const ChessboardMenu = () => {
             Ws.start(variantConst.CLASSICAL, modeConst.FEN);
           }}
         >
-          <RestartAltIcon size="small" />&nbsp;Start Classical
+          <RestartAltIcon size="small" />&nbsp;Classical
         </MenuItem>
         <MenuItem
           id="Nav-analysisBoard-MenuItem-startFischerRandom960"
@@ -70,7 +70,7 @@ const ChessboardMenu = () => {
             Ws.start(variantConst.CHESS_960, modeConst.FEN);
           }}
         >
-          <ShuffleIcon size="small" />&nbsp;Start Fischer Random
+          <ShuffleIcon size="small" />&nbsp;Fischer Random
         </MenuItem>
         <MenuItem
           id="Nav-analysisBoard-MenuItem-startCapablanca"
@@ -81,7 +81,7 @@ const ChessboardMenu = () => {
             Ws.start(variantConst.CAPABLANCA, modeConst.FEN);
           }}
         >
-          <BlurOnIcon size="small" />&nbsp;Start Capablanca
+          <BlurOnIcon size="small" />&nbsp;Capablanca
         </MenuItem>
         <Divider />
         <MenuItem
