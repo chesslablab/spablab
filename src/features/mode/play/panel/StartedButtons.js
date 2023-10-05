8618527c6ef491ce76e8db11fd30edb37d2596b3
programarivm <info@programarivm.com>
2023-10-05 18:34:25 +0200

    Refactored StartedButtons

--- src/features/mode/play/panel/StartedButtons.js
+++ src/features/mode/play/panel/StartedButtons.js
@@ -6,55 +6,56 @@ import * as playMode from 'features/mode/playModeSlice';
 import Ws from 'features/ws/Ws';
 
 const StartedButtons = () => {
-  const state = useSelector(state => state);
+  const stateBoard = useSelector(state => state.board);
+
+  const statePlayMode = useSelector(state => state.playMode);
+
   const dispatch = useDispatch();
 
-  const disabled = state.board.isMate ||
-    state.board.isStalemate ||
-    state.playMode.draw === Wording.verb.ACCEPT.toLowerCase() ||
-    state.playMode.takeback === Wording.verb.PROPOSE.toLowerCase() ||
-    state.playMode.resign === Wording.verb.ACCEPT.toLowerCase() ||
-    state.playMode.leave ||
-    state.playMode.timeOut;
+  const disabled = stateBoard.isMate ||
+    stateBoard.isStalemate ||
+    statePlayMode.draw === Wording.verb.ACCEPT.toLowerCase() ||
+    statePlayMode.takeback === Wording.verb.PROPOSE.toLowerCase() ||
+    statePlayMode.resign === Wording.verb.ACCEPT.toLowerCase() ||
+    statePlayMode.leave ||
+    statePlayMode.timeOut;
 
-  if (state.playMode.active) {
-    if (state.playMode.accepted) {
-      return (
-        <ButtonGroup
-          sx={{ mt: 1.5 }}
-          size="small"
-          aria-label="Game Buttons"
-          orientation="vertical"
-          fullWidth={true}
-          disabled={disabled}
-        >
-          <Button onClick={() => {
-            dispatch(infoAlert.show({
-              mssg: 'Waiting for the opponent to accept or decline.'
-            }));
-            dispatch(playMode.proposeTakeback());
-            Ws.takeback(Wording.verb.PROPOSE.toLowerCase());
-          }}>
-            Propose a takeback
-          </Button>
-          <Button onClick={() => {
-            dispatch(infoAlert.show({
-              mssg: 'Waiting for the opponent to accept or decline.'
-            }));
-            dispatch(playMode.proposeDraw());
-            Ws.draw(Wording.verb.PROPOSE.toLowerCase());
-          }}>
-            Offer draw
-          </Button>
-          <Button onClick={() => {
-            Ws.resign(Wording.verb.ACCEPT.toLowerCase());
-            dispatch(playMode.acceptResign());
-          }}>
-            Resign
-          </Button>
-        </ButtonGroup>
-      );
-    }
+  if (statePlayMode.accepted) {
+    return (
+      <ButtonGroup
+        sx={{ mt: 1.5 }}
+        size="small"
+        aria-label="Game Buttons"
+        orientation="vertical"
+        fullWidth={true}
+        disabled={disabled}
+      >
+        <Button onClick={() => {
+          dispatch(infoAlert.show({
+            mssg: 'Waiting for the opponent to accept or decline.'
+          }));
+          dispatch(playMode.proposeTakeback());
+          Ws.takeback(Wording.verb.PROPOSE.toLowerCase());
+        }}>
+          Propose a takeback
+        </Button>
+        <Button onClick={() => {
+          dispatch(infoAlert.show({
+            mssg: 'Waiting for the opponent to accept or decline.'
+          }));
+          dispatch(playMode.proposeDraw());
+          Ws.draw(Wording.verb.PROPOSE.toLowerCase());
+        }}>
+          Offer draw
+        </Button>
+        <Button onClick={() => {
+          Ws.resign(Wording.verb.ACCEPT.toLowerCase());
+          dispatch(playMode.acceptResign());
+        }}>
+          Resign
+        </Button>
+      </ButtonGroup>
+    );
   }
 
   return null;
