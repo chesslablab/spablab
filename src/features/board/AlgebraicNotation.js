75aab37c67093179133acf809462f530e02b33aa
programarivm <info@programarivm.com>
2023-10-05 16:40:17 +0200

    Refactored AlgebraicNotation

--- src/features/board/AlgebraicNotation.js
+++ src/features/board/AlgebraicNotation.js
@@ -2,32 +2,32 @@ import { useSelector } from 'react-redux';
 import Pgn from 'common/Pgn';
 
 const AlgebraicNotation = ({props}) => {
-  const state = useSelector(state => state);
-  
+  const state = useSelector(state => state.board);
+
   const spans = [];
 
-  if (props.i === state.board.size.ranks - 1 && state.board.flip === Pgn.symbol.WHITE) {
+  if (props.i === state.size.ranks - 1 && state.flip === Pgn.symbol.WHITE) {
     spans.push(<span key={spans.length} className="file">
       {props.sq.charAt(0)}
     </span>
     );
   }
 
-  if (props.j === 0 && state.board.flip === Pgn.symbol.WHITE) {
+  if (props.j === 0 && state.flip === Pgn.symbol.WHITE) {
     spans.push(<span key={spans.length} className="rank">
       {props.sq.charAt(1)}
     </span>
     );
   }
 
-  if (props.i === 0 && state.board.flip === Pgn.symbol.BLACK) {
+  if (props.i === 0 && state.flip === Pgn.symbol.BLACK) {
     spans.push(<span key={spans.length} className="file">
       {props.sq.charAt(0)}
     </span>
     );
   }
 
-  if (props.j === state.board.size.files - 1 && state.board.flip === Pgn.symbol.BLACK) {
+  if (props.j === state.size.files - 1 && state.flip === Pgn.symbol.BLACK) {
     spans.push(<span key={spans.length} className="rank">
       {props.sq.charAt(1)}
     </span>
