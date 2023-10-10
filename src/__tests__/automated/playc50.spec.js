c98fafa44dd7556a184d330fe7716b0605c700c4
programarivm <info@programarivm.com>
2023-10-10 18:23:14 +0200

    Transcribed test

---
+++ src/__tests__/automated/playc50.spec.js
@@ -0,0 +1,40 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const assert = require('assert');
+
+describe('Play C50', function() {
+  jest.setTimeout(30000);
+  let driver;
+  let vars;
+
+  beforeEach(async function() {
+    driver = await new Builder().forBrowser('chrome').build();
+    vars = {};
+  });
+
+  afterEach(async function() {
+    await driver.quit();
+  });
+
+  it('Play C50', async function() {
+    await driver.get("https://www.chesslablab.com:9443/");
+    await driver.wait(until.elementLocated(By.css(".e2 > img"))).click();
+    await driver.sleep(1000);
+    await driver.findElement(By.css(".e4")).click()
+    await driver.sleep(1000);
+    await driver.findElement(By.css(".e7 > img")).click()
+    await driver.sleep(1000);
+    await driver.findElement(By.css(".e5")).click()
+    await driver.sleep(1000);
+    await driver.findElement(By.css(".g1 > img")).click()
+    await driver.sleep(1000);
+    await driver.findElement(By.css(".f3")).click()
+    await driver.sleep(1000);
+    await driver.findElement(By.css(".b8 > img")).click()
+    await driver.sleep(1000);
+    await driver.findElement(By.css(".c6")).click()
+    await driver.sleep(1000);
+    await driver.findElement(By.css(".f1 > img")).click()
+    await driver.sleep(1000);
+    await driver.findElement(By.css(".c4")).click()
+  });
+});
