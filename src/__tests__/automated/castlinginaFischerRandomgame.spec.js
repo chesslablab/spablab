c949cab955866de53a44aabe05bc2ba497c96afd
dotarjun <arjunsingh8112@gmail.com>
2023-10-11 23:03:34 +0530

    refactor: add sleep
    Signed-off-by: dotarjun <arjunsingh8112@gmail.com>

--- src/__tests__/automated/castlinginaFischerRandomgame.spec.js
+++ src/__tests__/automated/castlinginaFischerRandomgame.spec.js
@@ -11,41 +11,24 @@ describe("Castling in a Fischer Random game", function () {
   });
   it("Castling in a Fischer Random game", async function () {
     await driver.get("https://www.chesslablab.com:9443/");
-    await driver.manage().window().setRect({ width: 1366, height: 742 });
-    await driver.findElement(By.id("Nav-analysisBoard")).click();
-    await driver
-      .findElement(By.id("Nav-analysisBoard-MenuItem-fenString"))
-      .click();
-    await driver.findElement(By.id("LoadFenDialog-TextField-fen")).click();
-    await driver
-      .findElement(By.id("LoadFenDialog-TextField-fen"))
-      .sendKeys("rnbqkbrn/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBRN w KQkq -");
-    {
-      const element = await driver.findElement(
-        By.id("LoadFenDialog-TextField-variant")
-      );
-      await driver
-        .actions({ bridge: true })
-        .moveToElement(element)
-        .clickAndHold()
-        .perform();
-    }
-    await driver
-      .findElement(By.id("LoadFenDialog-TextField-variant-MenuItem-960"))
-      .click();
-    await driver
-      .findElement(By.id("LoadFenDialog-TextField-startPos"))
-      .sendKeys("RNBQKBRN");
-    await driver.findElement(By.id("LoadFenDialog-Button-load")).click();
     await driver.findElement(By.css(".e2 > img")).click();
+    await driver.sleep(1000);
     await driver.findElement(By.css(".e4")).click();
+    await driver.sleep(1000);
     await driver.findElement(By.css(".b8 > img")).click();
+    await driver.sleep(1000);
     await driver.findElement(By.css(".c6")).click();
+    await driver.sleep(1000);
     await driver.findElement(By.css(".f1 > img")).click();
+    await driver.sleep(1000);
     await driver.findElement(By.css(".c4")).click();
+    await driver.sleep(1000);
     await driver.findElement(By.css(".b7 > img")).click();
+    await driver.sleep(1000);
     await driver.findElement(By.css(".b6")).click();
+    await driver.sleep(1000);
     await driver.findElement(By.css(".e1")).click();
+    await driver.sleep(1000);
     await driver.findElement(By.css(".g1")).click();
   });
 });
