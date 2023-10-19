3bad2bf909e71f555cc7135036b200de83846529
programarivm <info@programarivm.com>
2023-10-19 16:38:41 +0200

    Added BlackArchbishop.js

---
+++ src/features/board/piece/BlackArchbishop.js
@@ -0,0 +1,55 @@
+const BlackArchbishop = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={45} height={45} {...props}>
+    <path
+      fillRule="evenodd"
+      stroke="#000"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M36 36c-3.385-.972-10.115.43-13.5-2-3.385 2.43-10.115 1.028-13.5 2 0 0-1.646.542-3 2 .677.972 1.646.986 3 .5 3.385-.972 10.115.458 13.5-1 3.385 1.458 10.115.028 13.5 1 1.354.486 2.323.472 3-.5-1.354-1.945-3-2-3-2z"
+    />
+    <path
+      fillRule="evenodd"
+      stroke="#000"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M30 32c-2.5 2.5-12.5 2.5-15 0-.5-1.5 0-2 0-2h15s.5.5 0 2z"
+    />
+    <path fill="none" stroke="#000" strokeWidth={1.5} d="M30 30H15" />
+    <g strokeLinecap="round">
+      <path
+        fillRule="evenodd"
+        stroke="#000"
+        strokeWidth={1.14813}
+        d="M20.345 7.627c8.036.765 12.629 6.123 12.246 22.197H14.987c0-6.889 7.654-4.975 6.123-16.074"
+      />
+      <path
+        fillRule="evenodd"
+        stroke="#000"
+        strokeLinejoin="round"
+        strokeWidth={1.14813}
+        d="M21.875 13.75c.295 2.229-4.25 5.64-6.123 6.889-2.296 1.53-2.158 3.324-3.827 3.062-.797-.723 1.082-2.325 0-2.297-.765 0 .143.943-.765 1.531-.766 0-3.065.766-3.062-3.061 0-1.531 4.592-9.185 4.592-9.185s1.444-1.456 1.531-2.68c-.556-.76-.383-1.53-.383-2.296.766-.765 2.297 1.914 2.297 1.914h1.53s.599-1.525 1.914-2.296c.766 0 .766 2.296.766 2.296"
+      />
+      <path
+        stroke="#fff"
+        strokeLinejoin="round"
+        strokeWidth={1.14813}
+        d="M10.777 19.49a.383.383 0 1 1-.766 0 .383.383 0 1 1 .766 0zM14.936 12.028a.383 1.148 30 1 1-.663-.382.383 1.148 30 1 1 .663.382z"
+      />
+      <path
+        fillRule="evenodd"
+        stroke="#fff"
+        strokeWidth={0.76542}
+        d="M31.826 29.824c.765-15.308-4.21-21.049-9.185-21.814"
+      />
+    </g>
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M15 30h15"
+    />
+  </svg>
+);
+
+export default BlackArchbishop;
