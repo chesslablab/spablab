d407facdef2a3ee2dc8ad8d47ab82ad362bc7385
programarivm <info@programarivm.com>
2023-10-19 17:35:38 +0200

    Implemented SvgBlackChancellor

---
+++ src/features/board/piece/SvgBlackChancellor.js
@@ -0,0 +1,56 @@
+const SvgBlackChancellor = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={45} height={45} {...props}>
+    <path
+      fillRule="evenodd"
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="m34 34.82-3-3H14l-3 3"
+    />
+    <path
+      fillRule="evenodd"
+      stroke="#000"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M11 34.82v5h4v-2h5v2h5v-2h5v2h4v-5"
+    />
+    <path
+      fillRule="evenodd"
+      stroke="#000"
+      strokeWidth={1.5}
+      d="M31.553 32.109v-2.115H13.448v2.115"
+    />
+    <path fill="none" stroke="#fff" strokeLinecap="round" d="M11 34.82h23" />
+    <g strokeLinecap="round">
+      <path
+        fillRule="evenodd"
+        stroke="#000"
+        strokeWidth={1.14813}
+        d="M20.345 7.627c8.036.765 12.629 6.123 12.246 22.197H14.987c0-6.889 7.654-4.975 6.123-16.074"
+      />
+      <path
+        fillRule="evenodd"
+        stroke="#000"
+        strokeLinejoin="round"
+        strokeWidth={1.14813}
+        d="M21.875 13.75c.295 2.229-4.25 5.64-6.123 6.889-2.296 1.53-2.158 3.324-3.827 3.062-.797-.723 1.082-2.325 0-2.297-.765 0 .143.943-.765 1.531-.766 0-3.065.766-3.062-3.061 0-1.531 4.592-9.185 4.592-9.185s1.444-1.456 1.531-2.68c-.556-.76-.383-1.53-.383-2.296.766-.765 2.297 1.914 2.297 1.914h1.53s.599-1.525 1.914-2.296c.766 0 .766 2.296.766 2.296"
+      />
+      <path
+        stroke="#fff"
+        strokeLinejoin="round"
+        strokeWidth={1.14813}
+        d="M10.777 19.49a.383.383 0 1 1-.766 0 .383.383 0 1 1 .766 0zM14.936 12.028a.383 1.148 30 1 1-.663-.382.383 1.148 30 1 1 .663.382z"
+      />
+      <path
+        fill="none"
+        stroke="#fff"
+        strokeWidth={0.76542}
+        d="M31.826 29.824c.765-15.308-4.21-21.049-9.185-21.814"
+      />
+    </g>
+    <path fill="none" stroke="#fff" strokeWidth={1.5} d="M30 30H15" />
+  </svg>
+);
+
+export default SvgBlackChancellor;
--- src/features/board/piece/SvgPiece.js
+++ src/features/board/piece/SvgPiece.js
@@ -1,5 +1,6 @@
 import SvgBlackArchbishop from 'features/board/piece/SvgBlackArchbishop';
 import SvgBlackBishop from 'features/board/piece/SvgBlackBishop';
+import SvgBlackChancellor from 'features/board/piece/SvgBlackChancellor';
 
 export const SvgPiece = () => {
 
