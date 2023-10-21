7172981bc55def6587a1c86510afea4599ea1da8
programarivm <info@programarivm.com>
2023-10-21 20:38:43 +0200

    Removed ResizeSlider

--- src/features/board/Board.js
+++ src/features/board/Board.js
@@ -1,13 +1,11 @@
 import ActiveBoard from 'features/board/ActiveBoard';
 import PawnPromotionDialog from 'features/board/PawnPromotionDialog';
-import ResizeSlider from 'features/board/ResizeSlider';
 import BoardAudio from 'features/board/BoardAudio';
 
 const Board = () => {
   return (
     <>
       <ActiveBoard />
-      <ResizeSlider />
       <PawnPromotionDialog />
       <BoardAudio />
     </>
