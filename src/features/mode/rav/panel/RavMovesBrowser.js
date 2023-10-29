984f404c7d4e8cb684e0f292beea440ba0f79da0
programarivm <info@programarivm.com>
2023-10-29 15:17:51 +0100

    Updated to @chesslablab/reactblab 1.1.6

---
+++ src/features/mode/rav/panel/RavMovesBrowser.js
@@ -0,0 +1,53 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { HistoryButtons, RavMovesTable } from '@chesslablab/reactblab';
+import * as board from 'features/board/boardSlice';
+import * as panel from 'features/panel/panelSlice';
+
+const RavMovesBrowser = () => {
+  const stateBoard = useSelector(state => state.board);
+
+  const statePanel = useSelector(state => state.panel);
+
+  const stateRavMode = useSelector(state => state.ravMode);
+
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <HistoryButtons
+        stateHistoryButtons={{
+          back: statePanel.history.back,
+          fen: stateBoard.fen,
+        }}
+        onFastRewindClick={() => {
+          dispatch(panel.goTo({ back: stateBoard.fen.length - 1 }));
+        }}
+        onSkipPreviousClick={() => {
+          dispatch(panel.goBack());
+          dispatch(board.browseHistory());
+        }}
+        onSkipNextClick={() => {
+          dispatch(panel.goForward());
+          dispatch(board.browseHistory());
+        }}
+        onFastForwardClick={() => {
+          dispatch(panel.goToEnd());
+          dispatch(board.browseHistory());
+        }}
+      />
+      <RavMovesTable
+        stateRavMovesTable={{
+          back: statePanel.history.back,
+          fen: stateBoard.fen,
+          filtered: stateRavMode.filtered,
+          breakdown: stateRavMode.breakdown,
+        }}
+        onCellClick={(payload) => {
+          dispatch(panel.goTo(payload));
+        }}
+      />
+    </>
+  );
+}
+
+export default RavMovesBrowser;
