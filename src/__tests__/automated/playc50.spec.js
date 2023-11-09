2b67be5d4a3de2e088a21398a068a8e6001eac29
programarivm <info@programarivm.com>
2023-11-09 10:11:45 +0100

    Updated automated test

--- src/__tests__/automated/playc50.spec.js
+++ src/__tests__/automated/playc50.spec.js
@@ -17,23 +17,23 @@ describe('Play C50', function() {
 
   it('Play C50', async function() {
     await driver.get("https://www.chesslablab.com:9443/");
-    await driver.wait(until.elementLocated(By.css(".e2 > img"))).click();
+    await driver.wait(until.elementLocated(By.css(".e2 > svg"))).click();
     await driver.sleep(1000);
     await driver.findElement(By.css(".e4")).click()
     await driver.sleep(1000);
-    await driver.findElement(By.css(".e7 > img")).click()
+    await driver.findElement(By.css(".e7 > svg")).click()
     await driver.sleep(1000);
     await driver.findElement(By.css(".e5")).click()
     await driver.sleep(1000);
-    await driver.findElement(By.css(".g1 > img")).click()
+    await driver.findElement(By.css(".g1 > svg")).click()
     await driver.sleep(1000);
     await driver.findElement(By.css(".f3")).click()
     await driver.sleep(1000);
-    await driver.findElement(By.css(".b8 > img")).click()
+    await driver.findElement(By.css(".b8 > svg")).click()
     await driver.sleep(1000);
     await driver.findElement(By.css(".c6")).click()
     await driver.sleep(1000);
-    await driver.findElement(By.css(".f1 > img")).click()
+    await driver.findElement(By.css(".f1 > svg")).click()
     await driver.sleep(1000);
     await driver.findElement(By.css(".c4")).click()
   });
